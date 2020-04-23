b4292bbf82a1949a208b512d0616a9b62cacaf2c
Antonio Lucas Neres <antoniolucasnobar@gmail.com>
2020-04-23 18:10:46 -0300

    [T997] Simplificando buscar nome da tarefa

--- relatorios/consultas/T997-Remetidos-CEJUSC.sql
+++ relatorios/consultas/T997-Remetidos-CEJUSC.sql
@@ -11,19 +11,14 @@
   to_char(desloc.dt_deslocamento, 'dd/mm/yyyy hh24:mi') as "Envio ao CEJUSC / JAEP / Posto",
   to_char(desloc.dt_retorno, 'dd/mm/yyyy hh24:mi') as "Retorno do CEJUSC / JAEP / Posto",
   fase.nm_agrupamento_fase as "Fase",
-  t.ds_tarefa as "Tarefa"
+  ptar.nm_tarefa as "Tarefa"
   from tb_processo p
   join tb_processo_trf ptrf on p.id_processo = ptrf.id_processo_trf
+  join tb_processo_tarefa ptar on (p.id_processo = ptar.id_processo_trf)
   INNER JOIN pje.tb_classe_judicial cj ON (cj.id_classe_judicial = ptrf.id_classe_judicial)
   LEFT JOIN tb_hist_desloca_oj desloc ON p.id_processo::integer = desloc.id_processo_trf
   inner join tb_agrupamento_fase fase on (p.id_agrupamento_fase = fase.id_agrupamento_fase)
   join tb_orgao_julgador oj on (ptrf.id_orgao_julgador = oj.id_orgao_julgador)
-  join tb_processo_instance procxins on (p.id_processo = procxins.id_processo)
-  join jbpm_taskinstance ti on (procxins.id_proc_inst = ti.procinst_
-                                and ti.end_ is null
-                                and ti.isopen_ = 'true')
-  join tb_tarefa_jbpm tj on (tj.id_jbpm_task = ti.task_)
-  join tb_tarefa t on (tj.id_tarefa = t.id_tarefa)
   where 
     fase.id_agrupamento_fase = coalesce(:ID_FASE_PROCESSUAL, fase.id_agrupamento_fase)
     AND desloc.id_oj_origem  = coalesce(:ORGAO_JULGADOR_TODOS,desloc.id_oj_origem)
