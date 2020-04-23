2231c3f8b609b087ed926631cccbc7c68dd16db9
Antonio Lucas Neres <antoniolucasnobar@gmail.com>
2020-04-23 17:27:52 -0300

    [T997] Permitindo que consulte todos enviados para um posto:
    - ou entao todos que foram devolvidos para uma VT
    - alem disso, simplificando os nomes de VT e JAEP

--- relatorios/consultas/T997-Remetidos-CEJUSC.sql
+++ relatorios/consultas/T997-Remetidos-CEJUSC.sql
@@ -1,15 +1,15 @@
-  select 'http://processo='||p.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_DETALHES_PROCESSO' as " ",
+  SELECT 'http://processo='||p.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_DETALHES_PROCESSO' as " ",
         'http://processo='||p.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_TAREFA&texto='||cj.ds_classe_judicial_sigla||' '||p.nr_processo as "Processo",
 
-    (SELECT oj.ds_orgao_julgador
+    (SELECT REPLACE(oj.ds_orgao_julgador, 'VARA DO TRABALHO', 'VT' )
     FROM tb_orgao_julgador oj
     WHERE oj.id_orgao_julgador = desloc.id_oj_origem) as "Órgão Julgador",
 
-    (SELECT oj.ds_orgao_julgador
+    (SELECT REPLACE(oj.ds_orgao_julgador, 'JUÍZO AUXILIAR DE EXECUÇÃO E PRECATÓRIOS', 'JAEP' ) 
     FROM tb_orgao_julgador oj
     WHERE oj.id_orgao_julgador = desloc.id_oj_destino) as "CEJUSC / JAEP / Posto avançado",
-  to_char(desloc.dt_deslocamento, 'dd/mm/yyyy hh24:mi') as "Data envio ao Posto",
-  to_char(desloc.dt_retorno, 'dd/mm/yyyy hh24:mi') as "Data Retorno do Posto",
+  to_char(desloc.dt_deslocamento, 'dd/mm/yyyy hh24:mi') as "Envio ao CEJUSC / JAEP / Posto",
+  to_char(desloc.dt_retorno, 'dd/mm/yyyy hh24:mi') as "Retorno do CEJUSC / JAEP / Posto",
   fase.nm_agrupamento_fase as "Fase",
   t.ds_tarefa as "Tarefa"
   from tb_processo p
@@ -26,18 +26,18 @@
   join tb_tarefa t on (tj.id_tarefa = t.id_tarefa)
   where 
     fase.id_agrupamento_fase = coalesce(:ID_FASE_PROCESSUAL, fase.id_agrupamento_fase)
+    AND desloc.id_oj_origem  = coalesce(:ORGAO_JULGADOR_TODOS,desloc.id_oj_origem)
+    AND desloc.id_oj_destino = coalesce(:POSTO_CEJUSC,desloc.id_oj_destino)
     AND (
       ( -- remetidos
         (1 = :REMETIDO_DEVOLVIDO_POSTO)
         AND desloc.dt_deslocamento :: date between (:DATA_INICIAL)::date and (:DATA_FINAL)::date
         AND desloc.dt_retorno IS NULL
-        AND desloc.id_oj_origem  = coalesce(:ORGAO_JULGADOR_TODOS,desloc.id_oj_origem)
       ) 
       OR
       ( -- devolvidos
         (0 = :REMETIDO_DEVOLVIDO_POSTO)
         AND desloc.dt_retorno :: date between (:DATA_INICIAL)::date and (:DATA_FINAL)::date
-        AND desloc.id_oj_destino = coalesce(:POSTO_CEJUSC,desloc.id_oj_destino)
       )
     )
   order by p.nr_processo
