19ad24b079a4d2d278698c094f2c4effcebda382
Antonio Lucas Neres <antoniolucasnobar@gmail.com>
2020-04-27 11:42:35 -0300

    [T997] Correcao em nome de colunas e titulo

--- relatorios/consultas/T997-Remetidos-Posto-CEJUSC.sql
+++ relatorios/consultas/T997-Remetidos-Posto-CEJUSC.sql
@@ -1,17 +1,17 @@
 -- #Título
--- Processos Remetidos ou Devolvidos para Posto avançado (CEJUSC / JAEP)
+-- Processos Remetidos ou Devolvidos para Posto Avançado, CEJUSC ou JAEP
 
 -- #Código
 -- T997
 
 -- #Nome no menu
--- Processos Remetidos ou Devolvidos para Posto avançado (CEJUSC / JAEP)
+-- Processos Remetidos ou Devolvidos para Posto Avançado, CEJUSC ou JAEP
 
 -- #Menu superior
 -- Vara
 
 -- #Glossário
--- Lista os processos que foram enviados de um OJ para um Posto avançado, bem como os devolvidos do Posto avançado para OJ de origem.
+-- Lista os processos que foram enviados de uma VT para um Posto avançado, CEJUSC ou JAEP, bem como os devolvidos dessas unidades para a VT de origem
 -- <br />
 -- <br />
 -- <ul>
@@ -28,13 +28,13 @@
 
     (SELECT REPLACE(oj.ds_orgao_julgador, 'VARA DO TRABALHO', 'VT' )
     FROM tb_orgao_julgador oj
-    WHERE oj.id_orgao_julgador = desloc.id_oj_origem) as "Órgão Julgador",
+    WHERE oj.id_orgao_julgador = desloc.id_oj_origem) as "Unidade",
 
     (SELECT REPLACE(oj.ds_orgao_julgador, 'JUÍZO AUXILIAR DE EXECUÇÃO E PRECATÓRIOS', 'JAEP' ) 
     FROM tb_orgao_julgador oj
     WHERE oj.id_orgao_julgador = desloc.id_oj_destino) as "CEJUSC / JAEP / Posto avançado",
   to_char(desloc.dt_deslocamento, 'dd/mm/yyyy hh24:mi') as "Envio ao CEJUSC / JAEP / Posto",
-  to_char(desloc.dt_retorno, 'dd/mm/yyyy hh24:mi') as "Devolvido ao OJ",
+  to_char(desloc.dt_retorno, 'dd/mm/yyyy hh24:mi') as "Devolvido para VT",
   fase.nm_agrupamento_fase as "Fase",
   ptar.nm_tarefa as "Tarefa"
   from tb_processo p
