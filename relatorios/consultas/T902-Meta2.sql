b8a757f1a2abd76dd3dd44bede15d58e5e70aa6a
Antonio Lucas Neres <antoniolucasnobar@gmail.com>
2020-04-28 10:57:31 -0300

    [T902] Usando CASE para sobrestamento/suspensao

--- relatorios/consultas/T902-Meta2.sql
+++ relatorios/consultas/T902-Meta2.sql
@@ -7,14 +7,9 @@ SELECT 'http://processo='||p.nr_processo||'&grau=primeirograu&recurso=$RECURSO_P
     pe.dt_atualizacao as "Data Protocolo"
 
 FROM
-    -- (
-    --     SELECT pe.id_processo FROM tb_processo_evento pe 
-    --     INNER JOIN tb_evento_processual ev ON 
-    --         (pe.id_evento = ev.id_evento_processual AND ev.cd_evento = '26')
-    --     WHERE pe.dt_atualizacao < '01/01/2019'::date
-    -- )
     tb_processo_evento pe 
     INNER JOIN tb_evento_processual ev ON 
+    -- codigo 26 - distribuido
         (pe.id_evento = ev.id_evento_processual AND ev.cd_evento = '26')
     inner join tb_processo p on p.id_processo = pe.id_processo
     inner join tb_processo_trf ptrf on ptrf.id_processo_trf = p.id_processo   
@@ -70,39 +65,44 @@ WHERE
                 )
             END
     )
-    -- regra 1.c - Não tem movimento de sobrestamento/suspensao,
-    AND (NOT EXISTS 
-        (
-            SELECT pe.id_processo FROM tb_processo_evento pe 
-            INNER JOIN tb_evento_processual ev ON 
-                (pe.id_evento = ev.id_evento_processual)
-            WHERE p.id_processo = pe.id_processo
-            AND ev.cd_evento IN 
-                ('272', '275', '268', '12100', '50100', '898', 
-                '50092', '50135', '50136', '11975', '265', 
-                '50107', '11012', '11013', '11014', '11015')    
-        )
-        -- regra 1.c - ou, se tiver, tem tambem o de encerramento
-        OR (NOT EXISTS 
+    AND (CASE 
+         WHEN
+            -- regra 1.c - Não tem movimento de sobrestamento/suspensao,
+            NOT EXISTS 
             (
-                SELECT pe1.id_processo FROM tb_processo_evento pe1
+                SELECT pe.id_processo FROM tb_processo_evento pe 
                 INNER JOIN tb_evento_processual ev ON 
-                    (pe1.id_evento = ev.id_evento_processual)
-                WHERE p.id_processo = pe1.id_processo
+                    (pe.id_evento = ev.id_evento_processual)
+                WHERE p.id_processo = pe.id_processo
                 AND ev.cd_evento IN 
                     ('272', '275', '268', '12100', '50100', '898', 
                     '50092', '50135', '50136', '11975', '265', 
-                    '50107', '11012', '11013', '11014', '11015')  
-                AND EXISTS (
-                    SELECT pe2.id_processo FROM tb_processo_evento pe2
+                    '50107', '11012', '11013', '11014', '11015')    
+            )
+        THEN TRUE
+        ELSE
+        -- regra 1.c - ou, se tiver, tem tambem o de encerramento posteriormente
+         ( EXISTS 
+            (
+                SELECT pe2.id_processo FROM tb_processo_evento pe2
+                INNER JOIN tb_evento_processual ev ON 
+                    (pe2.id_evento = ev.id_evento_processual)
+                WHERE p.id_processo = pe2.id_processo
+                AND ev.cd_evento IN ('50054')
+                AND pe2.dt_atualizacao >
+                (
+                    SELECT MAX(pe.dt_atualizacao) FROM tb_processo_evento pe 
                     INNER JOIN tb_evento_processual ev ON 
-                        (pe2.id_evento = ev.id_evento_processual)
-                    WHERE p.id_processo = pe2.id_processo
-                    AND ev.cd_evento IN ('50054')
-                    AND pe2.dt_atualizacao > pe1.dt_atualizacao 
+                        (pe.id_evento = ev.id_evento_processual)
+                    WHERE p.id_processo = pe.id_processo
+                    AND ev.cd_evento IN 
+                        ('272', '275', '268', '12100', '50100', '898', 
+                        '50092', '50135', '50136', '11975', '265', 
+                        '50107', '11012', '11013', '11014', '11015') 
                 )
             )
         )
+        END
     )
         
 
