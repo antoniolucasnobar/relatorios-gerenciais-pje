9ed5d33f3c778f30fce17ef36ea14f4c0046434b
Antonio Lucas Neres <antoniolucasnobar@gmail.com>
2020-04-28 23:21:53 -0300

    [N992] Arquiv por movim e periodo cadastro CLE:
    - A maioria dos CLE foi cadastrada entre 01/09/2019 e 14/11/2019
    - esse periodo foi excluido
    - Não tava pegando os processos arquivados na nova tarefa.
    - Mudei para pegar por movimento

--- relatorios/consultas/N992-arquivados-sem-extincao-execucao.sql
+++ relatorios/consultas/N992-arquivados-sem-extincao-execucao.sql
@@ -2,29 +2,41 @@ with arquivados_definitivamente as (
    select
       p.id_processo,
       p.nr_processo,
-      ti.start_ dt_arquivamento,
-      oj.ds_orgao_julgador
-   from
-      tb_processo p
-   join
-      tb_processo_trf ptrf on (p.id_processo = ptrf.id_processo_trf)
-   join
-      tb_processo_instance procxins on (p.id_processo = procxins.id_processo)
-   join
-      jbpm_taskinstance ti on (procxins.id_proc_inst = ti.procinst_ and ti.end_ is null and ti.isopen_ = 'true')
-   join
-      tb_orgao_julgador oj on (oj.id_orgao_julgador = ptrf.id_orgao_julgador)
+      pe.dt_atualizacao dt_arquivamento,
+      oj.ds_orgao_julgador,
+      cj.ds_classe_judicial_sigla,
+      pt.nm_tarefa
+from tb_processo_evento pe 
+    INNER JOIN tb_evento_processual ev on (pe.id_evento = ev.id_evento_processual AND ev.cd_evento = '246')
+    inner join tb_processo p on p.id_processo = pe.id_processo
+    inner join tb_processo_trf ptrf on ptrf.id_processo_trf = p.id_processo   
+    join tb_orgao_julgador oj on (oj.id_orgao_julgador = ptrf.id_orgao_julgador)
+    inner join tb_processo_tarefa pt on pt.id_processo_trf = p.id_processo
+    INNER JOIN tb_classe_judicial cj ON (cj.id_classe_judicial = ptrf.id_classe_judicial)
    where
-      --Processo deve estar arquivado RN02 e RN03
-      ti.name_ in ('Arquivo definitivo', 'Arquivamento Definitivo')
-      and ti.start_ is not null
+      --Processo deve estar arquivado definitivamente RN02 e RN03
+    -- nao existe nenhum movimento de arquivamento ou desarquivamento posterior ao movimento escolhido
+    NOT EXISTS
+        ( SELECT 1
+        FROM tb_processo_evento prev
+        join tb_evento_processual ev 
+            on (prev.id_evento = ev.id_evento_processual)
+        where 
+            -- 246 - definitvamente, 245 -- provisoriamente
+            -- 893 - desarquivados os autos
+            ev.cd_evento IN ('246', '245', '893')
+            and prev.id_processo_evento_excludente is null
+            and prev.id_processo = p.id_processo
+            AND prev.dt_atualizacao > pe.dt_atualizacao
+        )
       and ptrf.id_orgao_julgador = coalesce(:ORGAO_JULGADOR_TODOS, ptrf.id_orgao_julgador) --Incluir o parâmetro de filtro OJ
 )
 select
    'http://processo='||ad.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_DETALHES_PROCESSO' as " ",
-   'http://processo='||ad.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_TAREFA&texto='||ad.nr_processo as "Processo",
-   ad.ds_orgao_julgador as "Órgão Julgador" ,
-   to_char(ad.dt_arquivamento, 'dd/mm/yyyy') as "Data do Arquivamento"
+   'http://processo='||ad.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_TAREFA&texto='||ad.ds_classe_judicial_sigla||' '||ad.nr_processo as "Processo",
+   REPLACE(ad.ds_orgao_julgador, 'VARA DO TRABALHO', 'VT' ) as "Órgão Julgador" ,
+   to_char(ad.dt_arquivamento, 'dd/mm/yyyy') as "Data do Arquivamento",
+   ad.nm_tarefa as "Tarefa"
 from
    arquivados_definitivamente ad
 where
@@ -63,5 +75,11 @@ where
          and prev.id_processo_evento_excludente is null
          and prev.id_processo = ad.id_processo
          and prev.dt_atualizacao < ad.dt_arquivamento
+   )
+      -- Assyst R133545
+   AND NOT EXISTS (
+       SELECT 1 FROM tb_processo_clet cle
+        WHERE cle.dt_inicio_clet:: date BETWEEN to_date('01/09/2019','DD/MM/YYYY') AND to_date('14/11/2019','DD/MM/YYYY')
+       and cle.id_processo_trf = ad.id_processo
    )
 order by ad.ds_orgao_julgador, ad.nr_processo
