82cd3ac03460fced38590a95213e6795e0cd7e51
Antonio Lucas Neres <antoniolucasnobar@gmail.com>
2020-04-30 12:45:38 -0300

    [T951] Atualizadas as regras:
    - se, em qq momento, tiver algum mov. de julgamento,
    sem mov. posterior de anulacao ou reforma, conta como julgado
    - se, depois da conclusao, tiver conversao em diligencia, tira pendencia
    - se tem nova conclusao pra sentenca, tira pendencia anterior

--- relatorios/consultas/T951-pendentes-detalhado.sql
+++ relatorios/consultas/T951-pendentes-detalhado.sql
@@ -1,5 +1,4 @@
-
-
+-- R136872 - Relatório SAO - SENTENÇAS DE CONHECIMENTO PENDENTES.
 WITH pendentes AS (
 SELECT  concluso.id_pessoa_magistrado, 
         pen.id_processo_evento,
@@ -25,33 +24,61 @@ SELECT  concluso.id_pessoa_magistrado,
                 (pe.id_evento = ev.id_evento_processual)
             WHERE pen.id_processo = pe.id_processo
             AND pe.id_processo_evento_excludente IS NULL
-            AND pe.dt_atualizacao > pen.dt_atualizacao
-            AND (ev.cd_evento IN 
+            AND (
+                (
+                    -- eh movimento de julgamento
+                    ev.cd_evento IN 
                     ('941', '442', '450', '452', '444', 
                     '471', '446', '448', '455', '466', 
                     '11795', '220', '50103', '221', '219', 
                     '472', '473', '458', '461', '459', '465', 
                     '462', '463', '457', '460', '464', '454'
-                    -- o movimento abaixo nao deve ser considerado para proferidas
-                    ,'11022' -- Convertido o julgamento em dilig_ncia 
                     )
+                    -- sem movimento de reforma/anulacao posterior
+                    AND 
+                    NOT EXISTS (
+                        SELECT 1 FROM 
+                        tb_processo_evento reforma_anulacao
+                        INNER JOIN tb_evento_processual ev 
+                            ON reforma_anulacao.id_evento = ev.id_evento_processual
+                        INNER JOIN tb_complemento_segmentado cs 
+                            ON (cs.id_movimento_processo = reforma_anulacao.id_evento)
+                        WHERE
+                            p.id_processo = reforma_anulacao.id_processo
+                            AND reforma_anulacao.id_processo_evento_excludente IS NULL
+                            AND pe.dt_atualizacao <= reforma_anulacao.dt_atualizacao
+                            AND ev.cd_evento = '132' 
+                            AND cs.ds_texto IN ('7098', '7131', '7132', '7467', '7585')
+                    )
+
+                )
                 OR
+                (
+                    pe.dt_atualizacao > pen.dt_atualizacao AND
                     (
-                        ev.cd_evento = '51' AND
-                        pe.ds_texto_final_interno ilike 'Concluso%proferir senten_a%'
+                        -- Convertido o julgamento em dilig_ncia 
+                        -- o movimento abaixo nao deve ser considerado para proferidas
+                        ev.cd_evento = '11022'
+                        OR
+                            (
+                                -- teve um novo concluso pra sentenca
+                                ev.cd_evento = '51' AND
+                                pe.ds_texto_final_interno ilike 'Concluso%proferir senten_a%'
+                            )
                     )    
                 )
+            ) 
         )
 )
  SELECT 'http://processo='||p.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_DETALHES_PROCESSO' as " ",
          'http://processo='||p.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_TAREFA&texto='
         --  ||cj.ds_classe_judicial_sigla||' '
          ||p.nr_processo as "Processo",
-ul.ds_nome,
+ul.ds_nome AS "Magistrado",
  p.pendente_desde AS "Pendente Desde"
 FROM pendentes p
 INNER JOIN tb_usuario_login ul ON (ul.id_usuario = p.id_pessoa_magistrado)
-ORDER BY ul.ds_nome
+ORDER BY ul.ds_nome, p.pendente_desde
 
 
 
