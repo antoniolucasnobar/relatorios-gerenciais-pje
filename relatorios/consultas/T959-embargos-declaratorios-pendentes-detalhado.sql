b4d5c63329ab92e2585c96bff50bf578db59e86c
Antonio Lucas Neres <antoniolucasnobar@gmail.com>
2020-05-07 11:30:10 -0300

    [T959] versao inicial baseada no T958

---
+++ relatorios/consultas/T959-embargos-declaratorios-pendentes-detalhado.sql
@@ -0,0 +1,83 @@
+-- R136874 - Relatório SAO -  EMBARGOS DECLARATÓRIOS PENDENTES detalhado
+-- REGRAS no T958
+
+-- explain analyze
+
+WITH 
+-- comentado pois jeferson falou que acontece da peticao estar classificada incorretamente.
+-- tipo_documento_embargo_declaracao AS (
+--     --23	Embargos de Declaração	S			49
+--     select id_tipo_processo_documento 
+--         from tb_tipo_processo_documento 
+--     where cd_documento = '49' 
+--         and in_ativo = 'S'
+-- ),
+pendentes_embargos_declaratorio AS (
+SELECT  concluso.id_pessoa_magistrado AS id_pessoa, 
+        pen.id_processo_evento,
+        pen.dt_atualizacao AS pendente_desde,
+        p.id_processo,
+        p.nr_processo
+    FROM 
+    tb_conclusao_magistrado concluso
+    INNER JOIN tb_processo_evento pen 
+        ON (pen.id_processo_evento = concluso.id_processo_evento 
+            AND pen.id_processo_evento_excludente IS NULL
+            -- esse é o codigo do movimento. se esse id mudar tem de ir na tb_evento_processual.cd_evento
+            and pen.id_evento = 51 
+            -- Conclusão do tipo "Julgamento dos Embargos de Declara__o" 
+            AND 
+                pen.ds_texto_final_interno ilike 
+                    'Conclusos os autos para julgamento dos Embargos de Declara__o%'
+        )
+    INNER JOIN tb_processo p on (p.id_processo = pen.id_processo)
+    -- INNER JOIN LATERAL (
+    --     SELECT doc.dt_juntada FROM tb_processo_documento doc WHERE 
+    --     doc.id_processo = pen.id_processo
+    --     AND doc.dt_juntada < pen.dt_atualizacao
+    --     AND doc.id_tipo_processo_documento IN (SELECT id_tipo_processo_documento FROM tipo_documento_embargo_declaracao)
+    --     ORDER BY doc.dt_juntada DESC LIMIT 1
+    -- ) peticao ON TRUE -- //ver comentario na definicao do tipo_documento_embargo_declaracao 
+    WHERE
+        concluso.id_pessoa_magistrado  = coalesce(:MAGISTRADO, concluso.id_pessoa_magistrado)
+        AND NOT EXISTS(
+            SELECT 1 FROM tb_processo_evento pe 
+            INNER JOIN tb_evento_processual ev ON 
+                (pe.id_evento = ev.id_evento_processual)
+            WHERE pen.id_processo = pe.id_processo
+                AND pe.id_processo_evento_excludente IS NULL
+                AND pe.dt_atualizacao > pen.dt_atualizacao
+                AND 
+                (
+                -- NÃO Existir um movimento dentre os seguintes, após o concluso
+                -- 50086 - Encerrada a conclusão
+                -- 198 - Acolhidos os Embargos de Declaração de #{nome da parte}  
+                -- 871 - Acolhidos em parte os Embargos de Declaração de #{nome da parte}  
+                -- 200 - Não acolhidos os Embargos de Declaração de #{nome da parte}  
+                -- 235 - Não conhecido(s) o(s) #{nome do recurso} / #{nome do conflito} de #{nome da parte} / #{nome da pessoa} 
+                -- 230 - Prejudicado(s) o(s) #{nome do recurso} de #{nome da parte}  
+                    ev.cd_evento IN 
+                    ('50086', '198', '871', '200', '235', '230')
+                    OR 
+                    (
+                        --nome do complemento bate com Embargos de Declara__o%
+                        ev.cd_evento IN ('235', '230') AND
+                        pe.ds_texto_final_interno ilike '%Embargos de Declara__o%'
+                    )
+                )  
+        )
+)
+SELECT 'http://processo='||p.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_DETALHES_PROCESSO' as " ",
+         'http://processo='||p.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_TAREFA&texto='
+         ||cj.ds_classe_judicial_sigla||' '
+         ||p.nr_processo as "Processo",
+    REPLACE(oj.ds_orgao_julgador, 'VARA DO TRABALHO', 'VT') AS "Unidade",
+    ul.ds_nome AS "Magistrado",
+    pendentes_embargos_declaratorio.pendente_desde AS "Pendente desde"
+FROM pendentes_embargos_declaratorio 
+    INNER JOIN tb_usuario_login ul on (ul.id_usuario = pendentes_embargos_declaratorio.id_pessoa)
+    INNER JOIN tb_processo p ON (p.id_processo = pendentes_embargos_declaratorio.id_processo)
+    inner join tb_processo_trf ptrf on ptrf.id_processo_trf = p.id_processo
+    inner join tb_orgao_julgador oj on oj.id_orgao_julgador = ptrf.id_orgao_julgador
+    INNER JOIN tb_classe_judicial cj ON (cj.id_classe_judicial = ptrf.id_classe_judicial)
+ORDER BY ul.ds_nome, pendentes_embargos_declaratorio.pendente_desde
