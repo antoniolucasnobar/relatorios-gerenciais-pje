e51e32f527a75664a0168968bfda7ca7af92242c
Antonio Lucas Neres <antoniolucasnobar@gmail.com>
2020-05-12 16:22:14 -0300

    [R136872][T950][T951] Classes de Conh./data opcion
    - São selecionadas apenas as classes de Conhecimento segundo o eGestão
    - A Data Final agora é opcional.
    - Quando o usuário não escolhe, fica a data atual.
    - Os seguintes movimentos retiram a pendencia:
    - Transito em julgado
    - redistribuido
    - Remetido ao 2 grau
    Desde que lancados apos o concluso.
    
    Add as seguintes colunas:
    - OJ
    - Tarefa Atual
    - classe judicial (junto com numero do processo)
    - Prioridades

--- relatorios/consultas/T950-sentenca-conhecimento-pendentes.sql
+++ relatorios/consultas/T950-sentenca-conhecimento-pendentes.sql
@@ -15,6 +15,55 @@ SELECT  concluso.id_pessoa_magistrado,
     	and pen.id_evento = 51 -- esse é o codigo do movimento. se esse id mudar tem de ir na tb_evento_processual.cd_evento
         AND pen.ds_texto_final_interno ilike 'Concluso%proferir senten_a%')
     INNER JOIN tb_processo p ON (p.id_processo = pen.id_processo)
+    inner join tb_processo_trf ptrf on ptrf.id_processo_trf = p.id_processo
+    INNER JOIN tb_classe_judicial cj 
+        ON (cj.id_classe_judicial = ptrf.id_classe_judicial
+            -- Segundo o e-Gestão, as classes da fase de CONHECIMENTO são essas:
+            -- 45	Ação de Exigir Contas
+            -- 32	Consignação em Pagamento
+            -- 37	Embargos de Terceiro Cível
+            -- 1709	Interdito Proibitório
+            -- 40	Monitória
+            -- 1707	Reintegração / Manutenção de Posse
+            -- 46	Restauração de Autos
+            -- 12374	Homologação da Transação Extrajudicial
+            -- 12227	Interpelação
+            -- 12226	Notificação
+            -- 12228	Protesto
+            -- 63	Ação Civil Coletiva
+            -- 65	Ação Civil Pública Cível
+            -- 74	Alvará Judicial - Lei 6858/80
+            -- 1269	Habeas Corpus Cível
+            -- 110	Habeas Data
+            -- 120	Mandado de Segurança Cível
+            -- 119	Mandado de Segurança Coletivo
+            -- 980	Ação de Cumprimento
+            -- 985	Ação Trabalhista - Rito Ordinário
+            -- 1126	Ação Trabalhista - Rito Sumário (Alçada)
+            -- 1125	Ação Trabalhista - Rito Sumaríssimo
+            -- 986	Inquérito para apuração de falta grave
+            -- 193	Produção Antecipada da Prova
+            -- 241	Petição Cível
+            -- 12135	Tutela Antecipada Antecedente
+            -- 12134	Tutela Cautelar Antecedente
+            -- 44	Prestação de Contas Oferecidas
+            -- 1295	Alvará Judicial
+            -- 178	Arresto
+            -- 180	Atentado
+            -- 181	Busca e Apreensão
+            -- 182	Caução
+            -- 183	Cautelar Inominada
+            -- 186	Exibição
+            -- 190	Justificação
+            -- 196	Sequestro
+            AND cj.cd_classe_judicial IN 
+                    ('45', '32', '37', '1709', '40', '1707', 
+                    '46', '12374', '12227', '12226', '12228', 
+                    '63', '65', '74', '1269', '110', '120', '119', 
+                    '980', '985', '1126', '1125', '986', '193', 
+                    '241', '12135', '12134', '44', '1295', '178', 
+                    '180', '181', '182', '183', '186', '190', '196')
+        )
     WHERE
         concluso.id_pessoa_magistrado  = coalesce(:MAGISTRADO, concluso.id_pessoa_magistrado)
         -- AND p.id_agrupamento_fase = 2  -- nao da pra usar a faase, pq tem de ver a situacao do processo na data escolhida pelo usuario.
@@ -24,7 +73,7 @@ SELECT  concluso.id_pessoa_magistrado,
             INNER JOIN tb_evento_processual ev ON 
                 (pe.id_evento = ev.id_evento_processual)
             WHERE pen.id_processo = pe.id_processo
-                AND pe.dt_atualizacao::date <= (:DATA_FINAL)::date
+                AND pe.dt_atualizacao::date <= (coalesce(:DATA_FINAL_OPCIONAL, current_date))::date
                 AND pe.id_processo_evento_excludente IS NULL
                 AND ev.cd_evento IN 
                     (
@@ -38,13 +87,13 @@ SELECT  concluso.id_pessoa_magistrado,
                 ORDER BY pe.dt_atualizacao DESC
                 LIMIT 1
         )
-        AND pen.dt_atualizacao::date <= (:DATA_FINAL)::date
+        AND pen.dt_atualizacao::date <= (coalesce(:DATA_FINAL_OPCIONAL, current_date))::date
         AND NOT EXISTS(
             SELECT 1 FROM tb_processo_evento pe 
             INNER JOIN tb_evento_processual ev ON 
                 (pe.id_evento = ev.id_evento_processual)
             WHERE pen.id_processo = pe.id_processo
-            AND pe.dt_atualizacao:: date <= (:DATA_FINAL)::date
+            AND pe.dt_atualizacao:: date <= (coalesce(:DATA_FINAL_OPCIONAL, current_date))::date
             AND pe.id_processo_evento_excludente IS NULL
             AND (
                 (
@@ -96,13 +145,21 @@ SELECT  concluso.id_pessoa_magistrado,
                     (
                         -- 941 - Declarada Incompetência
                         -- 11022 - Convertido o julgamento em dilig_ncia 
+                        -- TRANSITADO_EM_JULGADO ("848", "Transitado em julgado em #{data do trânsito}"),
+                        -- REDISTRIBUIDO ("36", "Redistribuído por #{tipo de redistribuição}  #{motivo da redistribuição}"),
                         -- esses movimentos nao devem ser considerado para proferidas
-                        ev.cd_evento IN ('941', '11022')
+                        ev.cd_evento IN ('941', '11022', '848', '36')
                         OR
                             (
                                 -- teve um novo concluso pra sentenca
                                 ev.cd_evento = '51' AND
                                 pe.ds_texto_final_interno ilike 'Concluso%proferir senten_a%'
+                            )
+                        OR
+                            (
+                                -- foi remetido ao 2 grau
+                                ev.cd_evento = '123' AND
+                                pe.ds_texto_final_interno ilike 'Remetidos os autos para Órgão jurisdicional competente%para processar recurso'
                             )
                     )    
                 )
@@ -113,7 +170,7 @@ SELECT ul.ds_nome AS "Magistrado",
 sentencas_conhecimento_pendentes_por_magistrado.pendentes_sentenca AS "Pendentes",
 '$URL/execucao/T951?MAGISTRADO='
 ||sentencas_conhecimento_pendentes_por_magistrado.id_pessoa_magistrado
-||'&DATA_FINAL='||to_char(:DATA_FINAL::date,'mm/dd/yyyy')
+||'&DATA_FINAL='||to_char((coalesce(:DATA_FINAL_OPCIONAL, current_date))::date,'mm/dd/yyyy')
 ||'&texto='||sentencas_conhecimento_pendentes_por_magistrado.pendentes_sentenca as "Ver Pendentes"
 FROM  (
     SELECT  sentencas_conhecimento_pendente.id_pessoa_magistrado, 
--- relatorios/consultas/T951-pendentes-detalhado.sql
+++ relatorios/consultas/T951-pendentes-detalhado.sql
@@ -1,7 +1,6 @@
 -- R136872 - Relatório SAO - SENTENÇAS DE CONHECIMENTO PENDENTES
 
-WITH 
-sentencas_conhecimento_pendentes_por_magistrado AS (
+WITH sentencas_conhecimento_pendentes_por_magistrado AS (
 SELECT  concluso.id_pessoa_magistrado, 
         pen.id_processo_evento,
         pen.dt_atualizacao AS pendente_desde,
@@ -15,6 +14,55 @@ SELECT  concluso.id_pessoa_magistrado,
     	and pen.id_evento = 51 -- esse é o codigo do movimento. se esse id mudar tem de ir na tb_evento_processual.cd_evento
         AND pen.ds_texto_final_interno ilike 'Concluso%proferir senten_a%')
     INNER JOIN tb_processo p ON (p.id_processo = pen.id_processo)
+    inner join tb_processo_trf ptrf on ptrf.id_processo_trf = p.id_processo
+    INNER JOIN tb_classe_judicial cj 
+        ON (cj.id_classe_judicial = ptrf.id_classe_judicial
+            -- Segundo o e-Gestão, as classes da fase de CONHECIMENTO são essas:
+            -- 45	Ação de Exigir Contas
+            -- 32	Consignação em Pagamento
+            -- 37	Embargos de Terceiro Cível
+            -- 1709	Interdito Proibitório
+            -- 40	Monitória
+            -- 1707	Reintegração / Manutenção de Posse
+            -- 46	Restauração de Autos
+            -- 12374	Homologação da Transação Extrajudicial
+            -- 12227	Interpelação
+            -- 12226	Notificação
+            -- 12228	Protesto
+            -- 63	Ação Civil Coletiva
+            -- 65	Ação Civil Pública Cível
+            -- 74	Alvará Judicial - Lei 6858/80
+            -- 1269	Habeas Corpus Cível
+            -- 110	Habeas Data
+            -- 120	Mandado de Segurança Cível
+            -- 119	Mandado de Segurança Coletivo
+            -- 980	Ação de Cumprimento
+            -- 985	Ação Trabalhista - Rito Ordinário
+            -- 1126	Ação Trabalhista - Rito Sumário (Alçada)
+            -- 1125	Ação Trabalhista - Rito Sumaríssimo
+            -- 986	Inquérito para apuração de falta grave
+            -- 193	Produção Antecipada da Prova
+            -- 241	Petição Cível
+            -- 12135	Tutela Antecipada Antecedente
+            -- 12134	Tutela Cautelar Antecedente
+            -- 44	Prestação de Contas Oferecidas
+            -- 1295	Alvará Judicial
+            -- 178	Arresto
+            -- 180	Atentado
+            -- 181	Busca e Apreensão
+            -- 182	Caução
+            -- 183	Cautelar Inominada
+            -- 186	Exibição
+            -- 190	Justificação
+            -- 196	Sequestro
+            AND cj.cd_classe_judicial IN 
+                    ('45', '32', '37', '1709', '40', '1707', 
+                    '46', '12374', '12227', '12226', '12228', 
+                    '63', '65', '74', '1269', '110', '120', '119', 
+                    '980', '985', '1126', '1125', '986', '193', 
+                    '241', '12135', '12134', '44', '1295', '178', 
+                    '180', '181', '182', '183', '186', '190', '196')
+        )
     WHERE
         concluso.id_pessoa_magistrado  = coalesce(:MAGISTRADO, concluso.id_pessoa_magistrado)
         -- AND p.id_agrupamento_fase = 2  -- nao da pra usar a faase, pq tem de ver a situacao do processo na data escolhida pelo usuario.
@@ -24,7 +72,7 @@ SELECT  concluso.id_pessoa_magistrado,
             INNER JOIN tb_evento_processual ev ON 
                 (pe.id_evento = ev.id_evento_processual)
             WHERE pen.id_processo = pe.id_processo
-                AND pe.dt_atualizacao::date <= (:DATA_FINAL)::date
+                AND pe.dt_atualizacao::date <= (coalesce(:DATA_FINAL_OPCIONAL, current_date))::date
                 AND pe.id_processo_evento_excludente IS NULL
                 AND ev.cd_evento IN 
                     (
@@ -38,13 +86,13 @@ SELECT  concluso.id_pessoa_magistrado,
                 ORDER BY pe.dt_atualizacao DESC
                 LIMIT 1
         )
-        AND pen.dt_atualizacao::date <= (:DATA_FINAL)::date
+        AND pen.dt_atualizacao::date <= (coalesce(:DATA_FINAL_OPCIONAL, current_date))::date
         AND NOT EXISTS(
             SELECT 1 FROM tb_processo_evento pe 
             INNER JOIN tb_evento_processual ev ON 
                 (pe.id_evento = ev.id_evento_processual)
             WHERE pen.id_processo = pe.id_processo
-            AND pe.dt_atualizacao:: date <= (:DATA_FINAL)::date
+            AND pe.dt_atualizacao:: date <= (coalesce(:DATA_FINAL_OPCIONAL, current_date))::date
             AND pe.id_processo_evento_excludente IS NULL
             AND (
                 (
@@ -96,13 +144,21 @@ SELECT  concluso.id_pessoa_magistrado,
                     (
                         -- 941 - Declarada Incompetência
                         -- 11022 - Convertido o julgamento em dilig_ncia 
+                        -- TRANSITADO_EM_JULGADO ("848", "Transitado em julgado em #{data do trânsito}"),
+                        -- REDISTRIBUIDO ("36", "Redistribuído por #{tipo de redistribuição}  #{motivo da redistribuição}"),
                         -- esses movimentos nao devem ser considerado para proferidas
-                        ev.cd_evento IN ('941', '11022')
+                        ev.cd_evento IN ('941', '11022', '848', '36')
                         OR
                             (
                                 -- teve um novo concluso pra sentenca
                                 ev.cd_evento = '51' AND
                                 pe.ds_texto_final_interno ilike 'Concluso%proferir senten_a%'
+                            )
+                        OR
+                            (
+                                -- foi remetido ao 2 grau
+                                ev.cd_evento = '123' AND
+                                pe.ds_texto_final_interno ilike 'Remetidos os autos para Órgão jurisdicional competente%para processar recurso'
                             )
                     )    
                 )
@@ -110,12 +166,28 @@ SELECT  concluso.id_pessoa_magistrado,
         )
 )
  SELECT 'http://processo='||p.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_DETALHES_PROCESSO' as " ",
-         'http://processo='||p.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_TAREFA&texto='
-        --  ||cj.ds_classe_judicial_sigla||' '
-         ||p.nr_processo as "Processo",
-ul.ds_nome AS "Magistrado",
- p.pendente_desde AS "Pendente Desde"
+        'http://processo='||p.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_TAREFA&texto='
+        ||cj.ds_classe_judicial_sigla||' '
+        ||p.nr_processo as "Processo",
+    REPLACE(oj.ds_orgao_julgador, 'VARA DO TRABALHO', 'VT') AS "Unidade",
+    ul.ds_nome AS "Magistrado",
+    p.pendente_desde AS "Pendente Desde",
+    pt.nm_tarefa as "Tarefa Atual",
+    (select COALESCE(string_agg(prioridade.ds_prioridade::character varying, ', '), '-')
+        from 
+        tb_proc_prioridde_processo tabela_ligacao 
+        inner join tb_prioridade_processo prioridade 
+            on (tabela_ligacao.id_prioridade_processo = prioridade.id_prioridade_processo)
+        where 
+        tabela_ligacao.id_processo_trf = p.id_processo
+    ) AS "Prioridades"
 FROM sentencas_conhecimento_pendentes_por_magistrado p
-INNER JOIN tb_usuario_login ul ON (ul.id_usuario = p.id_pessoa_magistrado)
+    INNER JOIN tb_usuario_login ul ON (ul.id_usuario = p.id_pessoa_magistrado)
+    inner join tb_processo_trf ptrf on ptrf.id_processo_trf = p.id_processo
+    INNER JOIN tb_classe_judicial cj ON (cj.id_classe_judicial = ptrf.id_classe_judicial)
+    inner join tb_orgao_julgador oj on oj.id_orgao_julgador = ptrf.id_orgao_julgador
+    inner join tb_processo_tarefa pt on pt.id_processo_trf = p.id_processo
 ORDER BY ul.ds_nome, p.pendente_desde 
+
+
 
