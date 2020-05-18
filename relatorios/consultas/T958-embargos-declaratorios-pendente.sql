add6c5cb26e795e97ee0f2e452de2b04e05e3edb
Antonio Lucas Neres <antoniolucasnobar@gmail.com>
2020-05-18 16:43:51 -0300

    [R136874][T958] nova regra pra peticao e + coisas:
    - para considerar a peticao, nao pode haver movimento de julgamento entre a mesma e o concluso para ED
    - sao considerados conclusos para julgamento e decisao de ED
    - considerando o mov. alterado o tipo de peticao para retirar pendencia (deveria considerar para colocar tb)
    - adicao da primeira linha com o total

--- relatorios/consultas/T958-embargos-declaratorios-pendente.sql
+++ relatorios/consultas/T958-embargos-declaratorios-pendente.sql
@@ -1,7 +1,7 @@
 -- R136874 - Relatório SAO -  EMBARGOS DECLARATÓRIOS PENDENTES
 -- REGRAS:
 -- Ter um concluso para:
---  Conclusos os autos para julgamento dos Embargos de Declaração a nome do Juiz
+--  Conclusos os autos para julgamento/Decisão dos Embargos de Declaração a nome do Juiz
 --
 -- Tem algo semelhante nesse????? Antes do concluso, ter um documento do tipo:
 --  - Embargos a Execucao
@@ -20,7 +20,6 @@
 
 
 WITH 
--- comentado pois jeferson falou que acontece da peticao estar classificada incorretamente.
 tipo_documento_embargo_declaracao AS (
     --23	Embargos de Declaração	S			49
     select id_tipo_processo_documento 
@@ -44,7 +43,7 @@ SELECT  concluso.id_pessoa_magistrado,
             -- Conclusão do tipo "Julgamento dos Embargos de Declara__o" 
             AND 
                 pen.ds_texto_final_interno ilike 
-                    'Conclusos os autos para julgamento dos Embargos de Declara__o%'
+                    'Conclusos os autos para%dos Embargos de Declara__o%'
             AND pen.dt_atualizacao::date <= (coalesce(:DATA_FINAL_OPCIONAL, current_date))::date
         )
     INNER JOIN tb_processo p on (p.id_processo = pen.id_processo)
@@ -53,6 +52,46 @@ SELECT  concluso.id_pessoa_magistrado,
         doc.id_processo = pen.id_processo
         AND doc.dt_juntada < pen.dt_atualizacao
         AND doc.id_tipo_processo_documento IN (SELECT id_tipo_processo_documento FROM tipo_documento_embargo_declaracao)
+        -- nao existe movimento de julgamento entre a peticao e a conclusao
+        AND NOT EXISTS(
+            SELECT 1 FROM tb_processo_evento pe 
+            INNER JOIN tb_evento_processual ev ON 
+                (pe.id_evento = ev.id_evento_processual)
+            WHERE pen.id_processo = pe.id_processo
+                AND pe.id_processo_evento_excludente IS NULL
+                AND pe.dt_atualizacao BETWEEN 
+                    doc.dt_juntada AND pen.dt_atualizacao
+                AND 
+                (
+                -- NÃO Existir um movimento dentre os seguintes, 
+                --  entre a data de juntada da peticao e o concluso
+                -- 50086 - Encerrada a conclusão
+                -- 198 - Acolhidos os Embargos de Declaração de #{nome da parte}  
+                -- 871 - Acolhidos em parte os Embargos de Declaração de #{nome da parte}  
+                -- 200 - Não acolhidos os Embargos de Declaração de #{nome da parte}  
+                -- 235 - Não conhecido(s) o(s) #{nome do recurso} / #{nome do conflito} de #{nome da parte} / #{nome da pessoa} 
+                -- 230 - Prejudicado(s) o(s) #{nome do recurso} de #{nome da parte}  
+                    ev.cd_evento = '50086'
+                    OR 
+                    (
+                        --nome do complemento bate com Embargos de Declara__o%
+                        ev.cd_evento IN ('198', '871', '200', '235', '230') AND
+                        pe.ds_texto_final_interno ilike '%Embargos de Declara__o%'
+                    )
+                    -- OR
+                    -- (
+                    --     ev.cd_evento = '51'AND
+                    --     pe.ds_texto_final_interno ilike 
+                    --         'Conclusos os autos para%dos Embargos de Declara__o%'
+                    -- )
+                    -- OR
+                    -- ( -- houve alteração do tipo de petição
+                    --     ev.cd_evento = '50088'AND
+                    --     pe.ds_texto_final_interno ilike 
+                    --         'Alterado o tipo de petição de Embargos de Declara__o%'
+                    -- )
+                )  
+        )
         ORDER BY doc.dt_juntada DESC LIMIT 1
     ) peticao ON TRUE -- //ver comentario na definicao do tipo_documento_embargo_declaracao 
     WHERE
@@ -86,21 +125,39 @@ SELECT  concluso.id_pessoa_magistrado,
                     (
                         ev.cd_evento = '51'AND
                         pe.ds_texto_final_interno ilike 
-                            'Conclusos os autos para julgamento dos Embargos de Declara__o%'
+                            'Conclusos os autos para%dos Embargos de Declara__o%'
+                    )
+                    OR
+                    ( -- houve alteração do tipo de petição
+                        ev.cd_evento = '50088'AND
+                        pe.ds_texto_final_interno ilike 
+                            'Alterado o tipo de petição de Embargos de Declara__o%'
                     )
                 )  
         )
 )
-SELECT ul.ds_nome AS "Magistrado", 
-embargos_declaratorios_pendentes.pendentes_embargo AS "Pendentes",
-'$URL/execucao/T959?MAGISTRADO='||embargos_declaratorios_pendentes.id_pessoa_magistrado
-||'&DATA_FINAL_OPCIONAL='||to_char((coalesce(:DATA_FINAL_OPCIONAL, current_date))::date,'mm/dd/yyyy')
-||'&texto='||embargos_declaratorios_pendentes.pendentes_embargo as "Ver Pendentes"
-FROM  (
+, embargos_declaratorios_pendentes AS (
     SELECT  pendentes_embargos_declaratorio.id_pessoa_magistrado, 
-            COUNT(pendentes_embargos_declaratorio.id_pessoa_magistrado) AS pendentes_embargo
+            COUNT(pendentes_embargos_declaratorio.id_pessoa_magistrado) AS pendentes_embargo,
+            MIN(pendentes_embargos_declaratorio.pendente_desde) AS pendente_mais_antigo
     FROM pendentes_embargos_declaratorio
     GROUP BY pendentes_embargos_declaratorio.id_pessoa_magistrado
-) embargos_declaratorios_pendentes  
-INNER JOIN tb_usuario_login ul ON (ul.id_usuario = embargos_declaratorios_pendentes.id_pessoa_magistrado)
-ORDER BY ul.ds_nome
+) 
+SELECT 
+    'TOTAL' AS "Magistrado", 
+    SUM(embargos_declaratorios_pendentes.pendentes_embargo) AS "Pendentes", 
+    '-' as "Ver Pendentes",
+    MIN(pendente_mais_antigo) - interval '1 milliseconds' AS "ED pendente mais antigo"       
+FROM embargos_declaratorios_pendentes
+UNION ALL
+(
+    SELECT ul.ds_nome AS "Magistrado", 
+    embargos_declaratorios_pendentes.pendentes_embargo AS "Pendentes",
+    '$URL/execucao/T959?MAGISTRADO='||embargos_declaratorios_pendentes.id_pessoa_magistrado
+    ||'&DATA_FINAL_OPCIONAL='||to_char((coalesce(:DATA_FINAL_OPCIONAL, current_date))::date,'mm/dd/yyyy')
+    ||'&texto='||embargos_declaratorios_pendentes.pendentes_embargo as "Ver Pendentes",
+    embargos_declaratorios_pendentes.pendente_mais_antigo AS "ED pendente mais antigo"
+    FROM embargos_declaratorios_pendentes  
+    INNER JOIN tb_usuario_login ul ON (ul.id_usuario = embargos_declaratorios_pendentes.id_pessoa_magistrado)
+    ORDER BY ul.ds_nome
+)
