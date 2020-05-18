b815f14d1e04644954ea212aa88135b90dd9bbad
Antonio Lucas Neres <antoniolucasnobar@gmail.com>
2020-05-18 16:56:49 -0300

    [R136873][T952][T953] inclui ata de audiencia:
    - tambem pode ser feito julgamento/acordo por ata de audienca
    - por isso retirou o concluso
    - separou os julgados dos acordos.
    - coluna prioridade retirada dos julgados, pois eh importante nos pendentes apenas.

--- relatorios/consultas/T952-sentenca-conhecimento-proferidas.sql
+++ relatorios/consultas/T952-sentenca-conhecimento-proferidas.sql
@@ -8,40 +8,36 @@ select
     doc.id_tipo_processo_documento
     from tb_processo_documento doc 
     inner join tb_proc_doc_bin_pess_assin assin on (doc.id_processo_documento_bin = assin.id_processo_documento_bin)
-    inner join lateral (
-        select pen.ds_texto_final_interno FROM 
-        tb_conclusao_magistrado concluso
-        INNER JOIN tb_processo_evento pen 
-            ON (pen.id_processo_evento = concluso.id_processo_evento 
-                and pen.id_processo = doc.id_processo 
-                AND pen.id_processo_evento_excludente IS NULL
-                and pen.id_evento = 51 -- esse é o codigo do movimento. se esse id mudar tem de ir na tb_evento_processual.cd_evento
-                -- esse filtro tem de ser feito depois, pois precisamos verificar se o concluso que antecedeu o documento eh de fato para sentenca
-                -- AND pen.ds_texto_final_interno ilike 'Concluso%julgamento%proferir senten_a%')
-            )
-        where pen.dt_atualizacao < doc.dt_juntada 
-        order by pen.dt_atualizacao desc 
-        limit 1
-    ) concluso on TRUE
     where doc.in_ativo = 'S'
-    -- 62 - sentenca,
-    -- 64 - decisao
-    AND doc.id_tipo_processo_documento IN (62, 64)
+    -- 93	Ata da Audiência	S			7298
+    -- 62	Sentença	S			7007
+    -- 64	Decisão	S			7001
+    AND doc.id_tipo_processo_documento IN (62, 64, 93)
     AND assin.id_pessoa = coalesce(:MAGISTRADO, assin.id_pessoa)
     -- and tipo.cd_documento = '7007'
     and doc.dt_juntada :: date between coalesce(:DATA_INICIAL_OPCIONAL, date_trunc('month', current_date))::date and (coalesce(:DATA_FINAL_OPCIONAL, current_date))::date
-    and concluso.ds_texto_final_interno 
-        ilike ANY (ARRAY[
-                'Concluso%julgamento%proferir senten_a%',
-                'Conclusos os autos para decis_o (gen_rica) a%'
-        ])
     --  nao pode ter "Extinta a execução ou o cumprimento da sentença por ..." lancado junto com a sentenca
     and not exists 
         (
             select 1 from tb_processo_evento extinta_execucao
-            where extinta_execucao.id_processo = doc.id_processo and 
-            date(doc.dt_juntada) = date(extinta_execucao.dt_atualizacao)
-            AND extinta_execucao.id_evento = 196
+                INNER JOIN tb_evento_processual ev ON 
+                    (extinta_execucao.id_evento = ev.id_evento_processual)
+            WHERE 
+                extinta_execucao.id_processo = doc.id_processo
+                AND extinta_execucao.dt_atualizacao BETWEEN 
+                    doc.dt_juntada - ('5 minutes')::interval
+                    AND doc.dt_juntada + ('5 minutes')::interval
+                -- 473 - Arquivado o processo por ausência do reclamante 
+                -- 472 - Arquivado o processo (Sumaríssimo - art. 852-B, § 1º, CLT) 
+                -- 463 - Extinto o processo por homologação de desistência 
+                -- 941 - Declarada Incompetência - 
+                -- 196 - Extinta a execução ou o cumprimento da sentença por #{motivo da extinção}
+                AND ev.cd_evento IN 
+                (
+                    '196', '473', '472', '463', '941'
+                )
+
+
         )
 )
 SELECT ul.ds_nome AS "Magistrado", 
@@ -66,15 +62,59 @@ FROM  (
             FILTER 
             (
                 WHERE 
-                    sentencas_conhecimento_proferidas.id_tipo_processo_documento = 62
-                    AND NOT EXISTS (
-                    SELECT 1 FROM tb_processo_evento acordo
-                    WHERE acordo.id_processo = sentencas_conhecimento_proferidas.id_processo
-                        AND acordo.dt_atualizacao::date = sentencas_conhecimento_proferidas.dt_juntada::date
-                        -- 377, Homologado o acordo em execução ou em cumprimento de sentença (valor do acordo: 1000,00)
-                        -- 466, Homologada a transação
-                        AND acordo.id_evento IN (377, 466)
-                    )
+                EXISTS (
+                    SELECT 1 FROM tb_processo_evento julgamento
+                        INNER JOIN tb_evento_processual ev ON 
+                            (julgamento.id_evento = ev.id_evento_processual)
+                        INNER JOIN tb_processo_trf ptrf ON ptrf.id_processo_trf = julgamento.id_processo
+                        INNER JOIN tb_classe_judicial cj 
+                            ON (cj.id_classe_judicial = ptrf.id_classe_judicial
+                            -- 116	Execução Fiscal
+                            -- 991	Execução de Termo de Ajuste de Conduta
+                            -- 992	Execução de Termo de Conciliação de CCP
+                            -- 990	Execução de Título Extrajudicial
+                            -- 993	Execução de Certidão de Crédito Judicial
+                            -- 994	Execução provisória em Autos Suplementares
+                            -- 156	Cumprimento de Sentença
+                                AND cj.cd_classe_judicial NOT IN 
+                                        ('116', '991', '992', '990', '993', '994', '156')
+                        )                    
+                        WHERE 
+                        julgamento.id_processo = sentencas_conhecimento_proferidas.id_processo
+                        AND julgamento.dt_atualizacao BETWEEN 
+                            sentencas_conhecimento_proferidas.dt_juntada - ('5 minutes')::interval
+                            AND sentencas_conhecimento_proferidas.dt_juntada + ('5 minutes')::interval
+                        AND ev.cd_evento IN 
+                            -- 442 - Concedida a segurança a "nome da parte"
+                            -- 450 - Concedida em parte a segurança a "nome da parte"
+                            -- 452 - Concedido em parte o Habeas Data a "nome da parte"
+                            -- 444 - Concedido o Habeas Data a "nome da parte"
+                            -- 471 - Declarada a decadência ou prescrição
+                            -- 446 - Denegada a segurança a "nome da parte"
+                            -- 448 - Denegado o Habeas Data a "nome da parte"
+                            -- 455 - Homologada a renúncia pelo autor
+                            -- 11795 - Homologado o reconhecimento da procedência do(s) pedido(s) de "nome da parte"
+                            -- 220 - Julgado(s) improcedente(s) o(s) pedido(s) ("classe processual" / "nome do incidente") de "nome da parte"
+                            -- 50103 - Julgado(s) liminarmente improcedente(s) o(s) pedido(s) ("classe processual" / "nome do incidente") de "nome da parte"
+                            -- 221 - Julgado(s) procedente(s) em parte o(s) pedido(s) ("classe processual" / "nome do incidente") de "nome da parte"
+                            -- 219 - Julgado(s) procedente(s) o(s) pedido(s) ("classe processual" / "nome do incidente") de "nome da parte"  
+                            -- 458 -  Extinto o processo por abandono da causa pelo autor
+                            -- 461 -  Extinto o processo por ausência de legitimidade ou de interesse processual
+                            -- 459 -  Extinto o processo por ausência de pressupostos processuais
+                            -- 465 -  Extinto o processo por confusão entre autor e o réu
+                            -- 462 -  Extinto o processo por convenção de arbitragem
+                            -- 457 -  Extinto o processo por negligência das partes
+                            -- 460 -  Extinto o processo por perempção, litispendência ou coisa julgada
+                            -- 464 -  Extinto o processo por ser a ação intransmissível
+                            -- 454 - Indeferida a petição inicial                        (
+                        (   
+                            '442', '450', '452', '444', 
+                            '471', '446', '448', '455', 
+                            '11795', '220', '50103', '221', '219', 
+                            '458', '461', '459', '465', 
+                            '462', '457', '460', '464', '454'
+                        )
+                )
             ) AS sentencas_conhecimento,
         COUNT(sentencas_conhecimento_proferidas.id_pessoa) 
             FILTER 
@@ -82,7 +122,9 @@ FROM  (
                 WHERE EXISTS (
                     SELECT 1 FROM tb_processo_evento acordo
                     WHERE acordo.id_processo = sentencas_conhecimento_proferidas.id_processo
-                        AND acordo.dt_atualizacao::date = sentencas_conhecimento_proferidas.dt_juntada::date
+                        AND acordo.dt_atualizacao BETWEEN 
+                            sentencas_conhecimento_proferidas.dt_juntada - ('5 minutes')::interval
+                            AND sentencas_conhecimento_proferidas.dt_juntada + ('5 minutes')::interval
                         -- 377, Homologado o acordo em execução ou em cumprimento de sentença (valor do acordo: 1000,00)
                         -- 466, Homologada a transação
                         AND acordo.id_evento IN (377, 466)
@@ -93,3 +135,8 @@ FROM  (
 ) sentencas_conhecimento  
 INNER JOIN tb_usuario_login ul ON (ul.id_usuario = sentencas_conhecimento.id_pessoa)
 ORDER BY ul.ds_nome
+
+
+
+
+
--- relatorios/consultas/T953-proferidos-detalhado.sql
+++ relatorios/consultas/T953-proferidos-detalhado.sql
@@ -1,7 +1,7 @@
 -- R136873 - Relatório SAO - SENTENÇAS DE CONHECIMENTO proferidas.
 
 -- explain analyze
-WITH sentencas_conhecimento_proferidas AS  (
+WITH sentencas_conhecimento_proferidas AS (
 select 
     assin.id_pessoa, 
     doc.dt_juntada,
@@ -9,40 +9,36 @@ select
     doc.id_tipo_processo_documento
     from tb_processo_documento doc 
     inner join tb_proc_doc_bin_pess_assin assin on (doc.id_processo_documento_bin = assin.id_processo_documento_bin)
-    inner join lateral (
-        select pen.ds_texto_final_interno FROM 
-        tb_conclusao_magistrado concluso
-        INNER JOIN tb_processo_evento pen 
-            ON (pen.id_processo_evento = concluso.id_processo_evento 
-                and pen.id_processo = doc.id_processo 
-                AND pen.id_processo_evento_excludente IS NULL
-                and pen.id_evento = 51 -- esse é o codigo do movimento. se esse id mudar tem de ir na tb_evento_processual.cd_evento
-                -- esse filtro tem de ser feito depois, pois precisamos verificar se o concluso que antecedeu o documento eh de fato para sentenca
-                -- AND pen.ds_texto_final_interno ilike 'Concluso%julgamento%proferir senten_a%')
-            )
-        where pen.dt_atualizacao < doc.dt_juntada 
-        order by pen.dt_atualizacao desc 
-        limit 1
-    ) concluso on TRUE
     where doc.in_ativo = 'S'
-    -- 62 - sentenca,
-    -- 64 - decisao
-    AND doc.id_tipo_processo_documento IN (62, 64)
+    -- 93	Ata da Audiência	S			7298
+    -- 62	Sentença	S			7007
+    -- 64	Decisão	S			7001
+    AND doc.id_tipo_processo_documento IN (62, 64, 93)
     AND assin.id_pessoa = coalesce(:MAGISTRADO, assin.id_pessoa)
     -- and tipo.cd_documento = '7007'
     and doc.dt_juntada :: date between coalesce(:DATA_INICIAL_OPCIONAL, date_trunc('month', current_date))::date and (coalesce(:DATA_FINAL_OPCIONAL, current_date))::date
-    and concluso.ds_texto_final_interno 
-        ilike ANY (ARRAY[
-                'Concluso%julgamento%proferir senten_a%',
-                'Conclusos os autos para decis_o (gen_rica) a%'
-        ])
     --  nao pode ter "Extinta a execução ou o cumprimento da sentença por ..." lancado junto com a sentenca
     and not exists 
         (
             select 1 from tb_processo_evento extinta_execucao
-            where extinta_execucao.id_processo = doc.id_processo and 
-            date(doc.dt_juntada) = date(extinta_execucao.dt_atualizacao)
-            AND extinta_execucao.id_evento = 196
+                INNER JOIN tb_evento_processual ev ON 
+                    (extinta_execucao.id_evento = ev.id_evento_processual)
+            WHERE 
+                extinta_execucao.id_processo = doc.id_processo
+                AND extinta_execucao.dt_atualizacao BETWEEN 
+                    doc.dt_juntada - ('5 minutes')::interval
+                    AND doc.dt_juntada + ('5 minutes')::interval
+                -- 473 - Arquivado o processo por ausência do reclamante 
+                -- 472 - Arquivado o processo (Sumaríssimo - art. 852-B, § 1º, CLT) 
+                -- 463 - Extinto o processo por homologação de desistência 
+                -- 941 - Declarada Incompetência - 
+                -- 196 - Extinta a execução ou o cumprimento da sentença por #{motivo da extinção}
+                AND ev.cd_evento IN 
+                (
+                    '196', '473', '472', '463', '941'
+                )
+
+
         )
 )
 SELECT 'http://processo='||p.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_DETALHES_PROCESSO' as " ",
@@ -52,15 +48,7 @@ SELECT 'http://processo='||p.nr_processo||'&grau=primeirograu&recurso=$RECURSO_P
     REPLACE(oj.ds_orgao_julgador, 'VARA DO TRABALHO', 'VT') AS "Unidade",
     ul.ds_nome AS "Magistrado",
     sentencas_conhecimento_proferidas.dt_juntada AS "Proferida em",
-    pt.nm_tarefa as "Tarefa Atual",
-    (select COALESCE(string_agg(prioridade.ds_prioridade::character varying, ', '), '-')
-        from 
-        tb_proc_prioridde_processo tabela_ligacao 
-        inner join tb_prioridade_processo prioridade 
-            on (tabela_ligacao.id_prioridade_processo = prioridade.id_prioridade_processo)
-        where 
-        tabela_ligacao.id_processo_trf = p.id_processo
-    ) AS "Prioridades"
+    pt.nm_tarefa as "Tarefa Atual"
 FROM sentencas_conhecimento_proferidas 
     INNER JOIN tb_usuario_login ul on (ul.id_usuario = sentencas_conhecimento_proferidas.id_pessoa)
     INNER JOIN tb_processo p ON (p.id_processo = sentencas_conhecimento_proferidas.id_processo)
@@ -73,25 +61,72 @@ WHERE
         -- Só Sentenças
         WHEN (:SENTENCA_OU_ACORDO = 1)
         THEN 
-            sentencas_conhecimento_proferidas.id_tipo_processo_documento = 62
-            AND NOT EXISTS (
-                SELECT 1 FROM tb_processo_evento acordo
-                WHERE acordo.id_processo = sentencas_conhecimento_proferidas.id_processo
-                  AND acordo.dt_atualizacao::date = sentencas_conhecimento_proferidas.dt_juntada::date
-                  -- 377, Homologado o acordo em execução ou em cumprimento de sentença (valor do acordo: 1000,00)
-                  -- 466, Homologada a transação
-                  AND acordo.id_evento IN (377, 466)
-            )
+            EXISTS (
+                    SELECT 1 FROM tb_processo_evento julgamento
+                        INNER JOIN tb_evento_processual ev ON 
+                            (julgamento.id_evento = ev.id_evento_processual)
+                        INNER JOIN tb_processo_trf ptrf ON ptrf.id_processo_trf = julgamento.id_processo
+                        INNER JOIN tb_classe_judicial cj 
+                            ON (cj.id_classe_judicial = ptrf.id_classe_judicial
+                            -- 116	Execução Fiscal
+                            -- 991	Execução de Termo de Ajuste de Conduta
+                            -- 992	Execução de Termo de Conciliação de CCP
+                            -- 990	Execução de Título Extrajudicial
+                            -- 993	Execução de Certidão de Crédito Judicial
+                            -- 994	Execução provisória em Autos Suplementares
+                            -- 156	Cumprimento de Sentença
+                                AND cj.cd_classe_judicial NOT IN 
+                                        ('116', '991', '992', '990', '993', '994', '156')
+                        )                    
+                        WHERE 
+                        julgamento.id_processo = sentencas_conhecimento_proferidas.id_processo
+                        AND julgamento.dt_atualizacao BETWEEN 
+                            sentencas_conhecimento_proferidas.dt_juntada - ('5 minutes')::interval
+                            AND sentencas_conhecimento_proferidas.dt_juntada + ('5 minutes')::interval
+                        AND ev.cd_evento IN 
+                            -- 442 - Concedida a segurança a "nome da parte"
+                            -- 450 - Concedida em parte a segurança a "nome da parte"
+                            -- 452 - Concedido em parte o Habeas Data a "nome da parte"
+                            -- 444 - Concedido o Habeas Data a "nome da parte"
+                            -- 471 - Declarada a decadência ou prescrição
+                            -- 446 - Denegada a segurança a "nome da parte"
+                            -- 448 - Denegado o Habeas Data a "nome da parte"
+                            -- 455 - Homologada a renúncia pelo autor
+                            -- 11795 - Homologado o reconhecimento da procedência do(s) pedido(s) de "nome da parte"
+                            -- 220 - Julgado(s) improcedente(s) o(s) pedido(s) ("classe processual" / "nome do incidente") de "nome da parte"
+                            -- 50103 - Julgado(s) liminarmente improcedente(s) o(s) pedido(s) ("classe processual" / "nome do incidente") de "nome da parte"
+                            -- 221 - Julgado(s) procedente(s) em parte o(s) pedido(s) ("classe processual" / "nome do incidente") de "nome da parte"
+                            -- 219 - Julgado(s) procedente(s) o(s) pedido(s) ("classe processual" / "nome do incidente") de "nome da parte"  
+                            -- 458 -  Extinto o processo por abandono da causa pelo autor
+                            -- 461 -  Extinto o processo por ausência de legitimidade ou de interesse processual
+                            -- 459 -  Extinto o processo por ausência de pressupostos processuais
+                            -- 465 -  Extinto o processo por confusão entre autor e o réu
+                            -- 462 -  Extinto o processo por convenção de arbitragem
+                            -- 457 -  Extinto o processo por negligência das partes
+                            -- 460 -  Extinto o processo por perempção, litispendência ou coisa julgada
+                            -- 464 -  Extinto o processo por ser a ação intransmissível
+                            -- 454 - Indeferida a petição inicial                        (
+                        (   
+                            '442', '450', '452', '444', 
+                            '471', '446', '448', '455', 
+                            '11795', '220', '50103', '221', '219', 
+                            '458', '461', '459', '465', 
+                            '462', '457', '460', '464', '454'
+                        )
+                )
         -- Só Acordos
         WHEN (:SENTENCA_OU_ACORDO = 2)
-        THEN EXISTS (
-                SELECT 1 FROM tb_processo_evento acordo
-                WHERE acordo.id_processo = sentencas_conhecimento_proferidas.id_processo
-                  AND acordo.dt_atualizacao::date = sentencas_conhecimento_proferidas.dt_juntada::date
-                  -- 377, Homologado o acordo em execução ou em cumprimento de sentença (valor do acordo: 1000,00)
-                  -- 466, Homologada a transação
-                  AND acordo.id_evento IN (377, 466)
-            )
+        THEN EXISTS 
+                (
+                    SELECT 1 FROM tb_processo_evento acordo
+                    WHERE acordo.id_processo = sentencas_conhecimento_proferidas.id_processo
+                        AND acordo.dt_atualizacao BETWEEN 
+                            sentencas_conhecimento_proferidas.dt_juntada - ('5 minutes')::interval
+                            AND sentencas_conhecimento_proferidas.dt_juntada + ('5 minutes')::interval
+                        -- 377, Homologado o acordo em execução ou em cumprimento de sentença (valor do acordo: 1000,00)
+                        -- 466, Homologada a transação
+                        AND acordo.id_evento IN (377, 466)
+                )
         -- Todos
         ELSE FALSE
     END
