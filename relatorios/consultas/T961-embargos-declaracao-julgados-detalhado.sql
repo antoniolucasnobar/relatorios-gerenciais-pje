e07d4929fa2672ad023ee4af0ba04662a7bcd59e
Antonio Lucas Neres <antoniolucasnobar@gmail.com>
2020-05-22 09:58:05 -0300

    [R136875][T961] batimento peticoes e julgam:
    - faz uma busca de cada petição de ED (ou alterada a petição p/ ED) e busca o primeiro movimento de julgado para bater. No final, recupera apenas aquelas que estejam com a data de julgamento dentro do intervalo escolhido pelo usuário

--- relatorios/consultas/T961-embargos-declaracao-julgados-detalhado.sql
+++ relatorios/consultas/T961-embargos-declaracao-julgados-detalhado.sql
@@ -3,8 +3,8 @@
 -- 871 - Acolhidos em parte os Embargos de Declaração de #{nome da parte}  
 -- 200 - Não acolhidos os Embargos de Declaração de #{nome da parte}  
 -- 235 - Não conhecido(s) o(s) #{nome do recurso} / #{nome do conflito} de #{nome da parte} / #{nome da pessoa} 
---  explain
-WITH
+-- explain
+WITH RECURSIVE
 tipo_documento_embargo_declaracao AS (
     --23	Embargos de Declaração	S			49
     select id_tipo_processo_documento 
@@ -35,12 +35,7 @@ embargos_declaracao_julgados AS (
     select
         assin.id_pessoa,
         doc.dt_juntada,
-        doc.id_processo,
---         movimento_julgamento_ed.ds_texto_final_interno,
-        juntadas.qtd as peticoes,
-        julgamentos_anteriores.qtd as solucoes_anteriores,
-        solucoes_atuais.qtd as solucoes_atuais,
-        LEAST((juntadas.qtd - julgamentos_anteriores.qtd), solucoes_atuais.qtd)  AS "EDs Julgados"
+        doc.id_processo
     from tb_processo_documento doc
              inner join tb_proc_doc_bin_pess_assin assin on (doc.id_processo_documento_bin = assin.id_processo_documento_bin)
              inner join lateral (
@@ -57,138 +52,127 @@ embargos_declaracao_julgados AS (
         order by pen.dt_atualizacao desc
         limit 1
         ) concluso ON TRUE
-             INNER JOIN LATERAL (
-        SELECT COUNT(*) AS qtd
-             , MIN(juntada.dt_atualizacao) AS data_juntada
-        FROM tb_processo_evento juntada
-        WHERE
-                juntada.id_processo = doc.id_processo
-          AND juntada.id_processo_evento_excludente IS NULL
-          AND juntada.dt_atualizacao < doc.dt_juntada
-          AND (
-                (juntada.id_evento = 85
-                    AND juntada.ds_texto_final_externo
-                     ilike 'Juntada a petição de Embargos de Declara__o%'
-                    )
-                OR
-                (juntada.id_evento = 50088
-                    AND juntada.ds_texto_final_externo
-                     ilike 'Alterado o tipo de peti__o de % para Embargos de Declara__o'
-                    )
-            )
-          AND NOT EXISTS(
-                SELECT 1
-                FROM tb_processo_evento pe
-                         INNER JOIN tb_evento_processual ev ON
-                    (pe.id_evento = ev.id_evento_processual)
-                WHERE doc.id_processo = pe.id_processo
-                  AND pe.id_processo_evento_excludente IS NULL
-                  AND pe.dt_atualizacao BETWEEN
-                    juntada.dt_atualizacao AND concluso.dt_atualizacao
-                  AND (
-                        --nome do complemento bate com Embargos de Declara__o%
-                         pe.id_evento IN
-                            (
-                            SELECT id_evento_processual
-                            FROM movimentos_embargos_declaracao_julgados
-                            )
-                         AND
-                            pe.ds_texto_final_interno ilike '%Embargos de Declara__o%'
-                    )
-            )
-        ) juntadas ON TRUE
-             INNER JOIN LATERAL (
-        SELECT COUNT(*) AS qtd
-        FROM tb_processo_evento julgamento
-        WHERE
-                julgamento.id_processo = doc.id_processo
-          AND julgamento.id_processo_evento_excludente IS NULL
-          AND julgamento.id_processo_documento IS DISTINCT FROM doc.id_processo_documento
-          AND julgamento.dt_atualizacao < COALESCE(juntadas.data_juntada, doc.dt_juntada)  - ('5 minutes')::interval
-          AND
-            (
-                    (julgamento.ds_texto_final_interno ilike '%Embargos de Declara__o%'
-                        AND julgamento.id_evento IN
-                            (
-                                SELECT id_evento_processual
-                                FROM movimentos_embargos_declaracao_julgados
-                            )
-                        )
-                    OR
-                    (julgamento.id_evento = 50088
-                        AND julgamento.ds_texto_final_externo
-                         ilike 'Alterado o tipo de peti__o de Embargos de Declara__o%'
-                        )
-                )
-        ) julgamentos_anteriores ON TRUE
-             INNER JOIN LATERAL (
-        SELECT COUNT(*) AS qtd FROM
-            tb_processo_evento pen
-        WHERE
-                pen.id_processo = doc.id_processo
-          AND pen.id_processo_evento_excludente IS NULL
-          and pen.id_evento IN (
-            SELECT id_evento_processual FROM movimentos_embargos_declaracao_julgados
-        )
-          AND (
-            pen.dt_atualizacao BETWEEN
-            doc.dt_juntada - ('5 minutes')::interval
-            AND doc.dt_juntada + ('5 minutes')::interval
---               date(doc.dt_juntada) <= date()
-              OR
-              pen.id_processo_documento = doc.id_processo_documento
-              )
-          AND  pen.ds_texto_final_interno ilike '%Embargos de Declara__o%'
-        ) solucoes_atuais ON TRUE
---              INNER JOIN LATERAL (
---         SELECT peti.dt_juntada AS data_peticao FROM tb_processo_documento peti WHERE
---                 peti.id_processo = doc.id_processo
---                                                                                  AND peti.dt_juntada < concluso.dt_atualizacao
---                                                                                  AND peti.id_tipo_processo_documento IN (SELECT id_tipo_processo_documento FROM tipo_documento_embargo_declaracao)
---                                                                                  -- nao existe movimento de julgamento entre a peticao e a conclusao
---                                                                                  AND NOT EXISTS(
---                     SELECT 1 FROM tb_processo_evento pe
---                                       INNER JOIN tb_evento_processual ev ON
---                         (pe.id_evento = ev.id_evento_processual)
---                     WHERE doc.id_processo = pe.id_processo
---                       AND pe.id_processo_evento_excludente IS NULL
---                       AND pe.dt_atualizacao BETWEEN
---                         peti.dt_juntada AND concluso.dt_atualizacao
---                       AND
---                         (
---                             -- NÃO Existir um movimento dentre os seguintes,
---                             --  entre a data de juntada da peticao e o concluso
---                             -- 198 - Acolhidos os Embargos de Declaração de #{nome da parte}
---                             -- 871 - Acolhidos em parte os Embargos de Declaração de #{nome da parte}
---                             -- 200 - Não acolhidos os Embargos de Declaração de #{nome da parte}
---                             -- 235 - Não conhecido(s) o(s) #{nome do recurso} / #{nome do conflito} de #{nome da parte} / #{nome da pessoa}
---                             (
---                                 --nome do complemento bate com Embargos de Declara__o%
---                                         ev.cd_evento IN ('198', '871', '200', '235') AND
---                                         pe.ds_texto_final_interno ilike '%Embargos de Declara__o%'
---                                 )
---                             )
---                 )
---         ORDER BY peti.dt_juntada DESC LIMIT 1
---         ) peticao ON TRUE
---              INNER JOIN LATERAL (
---         SELECT pen.ds_texto_final_interno FROM
---             tb_processo_evento pen
---         WHERE
---                 pen.id_processo = doc.id_processo
---           AND pen.id_processo_evento_excludente IS NULL
---           and pen.id_evento IN (
---             SELECT id_evento_processual FROM movimentos_embargos_declaracao_julgados
---         )
---           AND date(doc.dt_juntada) <= date(pen.dt_atualizacao)
---           AND  pen.ds_texto_final_interno ilike '%Embargos de Declara__o%'
---         ) movimento_julgamento_ed ON TRUE
+
     where doc.in_ativo = 'S'
       AND doc.id_tipo_processo_documento IN (SELECT id_tipo_processo_documento FROM tipo_documento_sentenca)
       AND assin.id_pessoa = coalesce(:MAGISTRADO, assin.id_pessoa)
       and doc.dt_juntada :: date between coalesce(:DATA_INICIAL_OPCIONAL, date_trunc('month', current_date))::date and (coalesce(:DATA_OPCIONAL_FINAL, current_date))::date
       and concluso.ds_texto_final_interno ilike 'Conclusos os autos para % dos Embargos de Declara__o%'
 )
+, peticoes_eds (id_processo, id_peticao, id_julgamento) AS (
+    (
+        SELECT DISTINCT ON (ed.id_processo) ed.id_processo
+                                          , ed.id_processo_evento             AS id_peticao
+                                          , julgamento.id_processo_evento     AS id_julgamento
+                                          , ed.dt_atualizacao                 AS data_ed
+                                          , ed.ds_texto_final_externo AS tx_ed
+                                          , julgamento.dt_atualizacao         AS dt_j
+                                          , julgamento.ds_texto_final_externo AS tx_j
+        FROM tb_processo_evento ed
+                 LEFT JOIN tb_processo_evento julgamento ON
+            (ed.id_processo = julgamento.id_processo
+                AND julgamento.dt_atualizacao > ed.dt_atualizacao
+                AND julgamento.dt_atualizacao::date <= COALESCE(:DATA_OPCIONAL_FINAL, CURRENT_DATE)::date
+                AND (
+                     (julgamento.ds_texto_final_interno ilike '%Embargos de Declara__o%'
+                         AND julgamento.id_evento IN
+                             (
+                                 SELECT id_evento_processual
+                                 FROM movimentos_embargos_declaracao_julgados
+                             )
+                         )
+                     OR
+                     (julgamento.id_evento = 50088
+                         AND julgamento.ds_texto_final_externo
+                          ilike
+                             'Alterado o tipo de peti__o de Embargos de Declara__o%'
+                         )
+                 )
+                )
+        WHERE ed.id_processo IN (
+                                 SELECT edj.id_processo FROM embargos_declaracao_julgados edj
+            )
+          AND ed.dt_atualizacao::date <= COALESCE(:DATA_OPCIONAL_FINAL, CURRENT_DATE)::date
+
+          AND (
+                (ed.id_evento = 85
+                    AND ed.ds_texto_final_externo
+                     ilike 'Juntada a petição de Embargos de Declara__o%'
+                    )
+                OR
+                (ed.id_evento = 50088
+                    AND ed.ds_texto_final_externo
+                     ilike 'Alterado o tipo de peti__o de % para Embargos de Declara__o'
+                    )
+            )
+        ORDER BY ed.id_processo, ed.dt_atualizacao, julgamento.id_processo_evento
+    )
+    UNION
+    (
+        SELECT DISTINCT ON (ed.id_processo) ed.id_processo
+                                          , ed.id_processo_evento             AS id_peticao
+                                          , julgamento.id_processo_evento     AS id_j
+                                          , ed.dt_atualizacao                 AS data_ed
+                                          , ed.ds_texto_final_externo AS tx_ed
+                                          , julgamento.dt_atualizacao         AS dt_j
+                                          , julgamento.ds_texto_final_externo AS tx_j
+        FROM peticoes_eds pj
+                 INNER JOIN tb_processo_evento ed ON (ed.id_processo = pj.id_processo)
+                 LEFT JOIN tb_processo_evento julgamento ON
+            (ed.id_processo = julgamento.id_processo
+                AND julgamento.id_processo_evento > pj.id_julgamento
+                AND julgamento.dt_atualizacao > ed.dt_atualizacao
+                AND julgamento.dt_atualizacao::date <= COALESCE(:DATA_OPCIONAL_FINAL, CURRENT_DATE)::date
+                AND (
+                     (julgamento.ds_texto_final_interno ilike '%Embargos de Declara__o%'
+                         AND julgamento.id_evento IN
+                             (
+                                 SELECT id_evento_processual
+                                 FROM movimentos_embargos_declaracao_julgados
+                             )
+                         )
+                     OR
+                     (julgamento.id_evento = 50088
+                         AND julgamento.ds_texto_final_externo
+                          ilike
+                             'Alterado o tipo de peti__o de Embargos de Declara__o%'
+                         )
+                 )
+                )
+        WHERE ed.id_processo_evento > id_peticao
+          AND ed.dt_atualizacao::date <= COALESCE(:DATA_OPCIONAL_FINAL, CURRENT_DATE)::date
+          AND ed.id_processo = pj.id_processo
+          AND (
+                (ed.id_evento = 85
+                    AND ed.ds_texto_final_externo
+                     ilike 'Juntada a petição de Embargos de Declara__o%'
+                    )
+                OR
+                (ed.id_evento = 50088
+                    AND ed.ds_texto_final_externo
+                     ilike 'Alterado o tipo de peti__o de % para Embargos de Declara__o'
+                    )
+            )
+        ORDER BY ed.id_processo, ed.dt_atualizacao, julgamento.id_processo_evento
+    )
+)
+, eds_julgados AS (
+    SELECT peticoes_eds.id_processo,
+           count(peticoes_eds.id_processo) AS numero_julgados
+    FROM peticoes_eds
+    WHERE peticoes_eds.dt_j::date BETWEEN
+              COALESCE(:DATA_INICIAL_OPCIONAL, date_trunc('month', current_date))::date
+              AND
+              COALESCE(:DATA_OPCIONAL_FINAL, CURRENT_DATE)::date
+    GROUP BY peticoes_eds.id_processo
+)
+-- , eds_por_magistrado AS (
+--     SELECT edj.id_pessoa,
+--            SUM(eds_julgados.numero_julgados) AS julgados_por_mag
+--     FROM embargos_declaracao_julgados edj
+--     INNER JOIN eds_julgados ON (eds_julgados.id_processo = edj.id_processo)
+--     GROUP BY edj.id_pessoa
+-- )
 SELECT 'http://processo='||p.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_DETALHES_PROCESSO' as " ",
          'http://processo='||p.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_TAREFA&texto='
          ||cj.ds_classe_judicial_sigla||' '
@@ -196,13 +180,17 @@ SELECT 'http://processo='||p.nr_processo||'&grau=primeirograu&recurso=$RECURSO_P
     REPLACE(oj.ds_orgao_julgador, 'VARA DO TRABALHO', 'VT') AS "Unidade",
     ul.ds_nome AS "Magistrado",
     embargos_declaracao_julgados.dt_juntada AS "Julgado em",
+    eds_julgados.numero_julgados AS "Quantidade EDs",
 --     embargos_declaracao_julgados.ds_texto_final_interno AS "Movimento de Solução",
-    pt.nm_tarefa as "Tarefa Atual",
-       embargos_declaracao_julgados.peticoes,
-       embargos_declaracao_julgados.solucoes_anteriores,
-       embargos_declaracao_julgados.solucoes_atuais,
-    embargos_declaracao_julgados."EDs Julgados"
+    pt.nm_tarefa as "Tarefa Atual"
+    ,
+   '$URL/execucao/T964?NUM_PROCESSO='||p.nr_processo
+       -- MAGISTRADO='||eds_julgados.id_pessoa
+       ||'&DATA_INICIAL_OPCIONAL='||to_char(coalesce(:DATA_INICIAL_OPCIONAL, date_trunc('month', current_date))::date,'mm/dd/yyyy')
+       ||'&DATA_OPCIONAL_FINAL='||to_char((coalesce(:DATA_OPCIONAL_FINAL, current_date))::date,'mm/dd/yyyy')
+       ||'&texto='||eds_julgados.numero_julgados as "Ver EDs Julgados do Processo"
 FROM embargos_declaracao_julgados
+    INNER JOIN eds_julgados ON (eds_julgados.id_processo = embargos_declaracao_julgados.id_processo)
     INNER JOIN tb_usuario_login ul on (ul.id_usuario = embargos_declaracao_julgados.id_pessoa)
     INNER JOIN tb_processo p ON (p.id_processo = embargos_declaracao_julgados.id_processo)
     inner join tb_processo_trf ptrf on ptrf.id_processo_trf = p.id_processo
