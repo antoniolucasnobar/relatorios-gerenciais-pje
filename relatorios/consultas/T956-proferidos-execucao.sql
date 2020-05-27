aef1d74e1e166344aebc5090e7b29dd360a7adf8
Antonio Lucas Neres <antoniolucasnobar@gmail.com>
2020-05-27 12:39:22 -0300

    [R136878][T956][T957] n conta concluso:
    - Jeferson explicou que os movimentos desses incidentes podem ser lancados de outros concluso

--- relatorios/consultas/T956-proferidos-execucao.sql
+++ relatorios/consultas/T956-proferidos-execucao.sql
@@ -32,19 +32,20 @@ movimentos_julgado_incidentes_execucao AS (
          doc.id_processo
      from tb_processo_documento doc
           inner join tb_proc_doc_bin_pess_assin assin on (doc.id_processo_documento_bin = assin.id_processo_documento_bin)
-          inner join lateral (
-            select pen.ds_texto_final_interno, pen.dt_atualizacao FROM
-             tb_conclusao_magistrado concluso
-                 INNER JOIN tb_processo_evento pen
-                        ON (pen.id_processo_evento = concluso.id_processo_evento
-                            and pen.id_processo = doc.id_processo
-                            AND pen.id_processo_evento_excludente IS NULL
-                            and pen.id_evento = 51 -- esse é o codigo do movimento. se esse id mudar tem de ir na tb_evento_processual.cd_evento
-                            )
-             where pen.dt_atualizacao < doc.dt_juntada
-             order by pen.dt_atualizacao desc
-             limit 1
-          ) concluso ON TRUE
+-- retirando o concluso a pedido de Jeferson, pois o Qliq so considera a peticao e o movimento de julgamento
+--           inner join lateral (
+--             select pen.ds_texto_final_interno, pen.dt_atualizacao FROM
+--              tb_conclusao_magistrado concluso
+--                  INNER JOIN tb_processo_evento pen
+--                         ON (pen.id_processo_evento = concluso.id_processo_evento
+--                             and pen.id_processo = doc.id_processo
+--                             AND pen.id_processo_evento_excludente IS NULL
+--                             and pen.id_evento = 51 -- esse é o codigo do movimento. se esse id mudar tem de ir na tb_evento_processual.cd_evento
+--                             )
+--              where pen.dt_atualizacao < doc.dt_juntada
+--              order by pen.dt_atualizacao desc
+--              limit 1
+--           ) concluso ON TRUE
 --         INNER JOIN tb_processo_evento iniciada_execucao
 --             ON (iniciada_execucao.id_processo = doc.id_processo
 --             AND iniciada_execucao.id_evento = 11385)
@@ -63,12 +64,12 @@ movimentos_julgado_incidentes_execucao AS (
       and doc.dt_juntada :: date between
           coalesce(:DATA_INICIAL_OPCIONAL,date_trunc('month', current_date))::date
           and (coalesce(:DATA_OPCIONAL_FINAL, current_date))::date
-
-      and concluso.ds_texto_final_interno ilike ANY (
-        ARRAY['Conclusos os autos para julgamento da ação incidental na execu__o%'
-             ,'Conclusos os autos para julgamento dos Embargos _ Execu__o%'
-             ,'Conclusos os autos para julgamento da Impugna__o _ Sentença de Liquida__o%']
-        )
+-- ver inner join do concluso
+--       and concluso.ds_texto_final_interno ilike ANY (
+--         ARRAY['Conclusos os autos para julgamento da ação incidental na execu__o%'
+--              ,'Conclusos os autos para julgamento dos Embargos _ Execu__o%'
+--              ,'Conclusos os autos para julgamento da Impugna__o _ Sentença de Liquida__o%']
+--         )
      -- TODO: pensar sobre se é necessário já testar os movimentos aqui
 --     AND EXISTS (
 --         SELECT 1 FROM
--- relatorios/consultas/T957-proferidos-execucao-detalhado.sql
+++ relatorios/consultas/T957-proferidos-execucao-detalhado.sql
@@ -32,19 +32,20 @@ WITH RECURSIVE
             doc.id_processo
         from tb_processo_documento doc
                  inner join tb_proc_doc_bin_pess_assin assin on (doc.id_processo_documento_bin = assin.id_processo_documento_bin)
-                 inner join lateral (
-            select pen.ds_texto_final_interno, pen.dt_atualizacao FROM
-                tb_conclusao_magistrado concluso
-                    INNER JOIN tb_processo_evento pen
-                               ON (pen.id_processo_evento = concluso.id_processo_evento
-                                   and pen.id_processo = doc.id_processo
-                                   AND pen.id_processo_evento_excludente IS NULL
-                                   and pen.id_evento = 51 -- esse é o codigo do movimento. se esse id mudar tem de ir na tb_evento_processual.cd_evento
-                                   )
-            where pen.dt_atualizacao < doc.dt_juntada
-            order by pen.dt_atualizacao desc
-            limit 1
-            ) concluso ON TRUE
+-- retirando o concluso a pedido de Jeferson, pois o Qliq so considera a peticao e o movimento de julgamento
+--           inner join lateral (
+--             select pen.ds_texto_final_interno, pen.dt_atualizacao FROM
+--              tb_conclusao_magistrado concluso
+--                  INNER JOIN tb_processo_evento pen
+--                         ON (pen.id_processo_evento = concluso.id_processo_evento
+--                             and pen.id_processo = doc.id_processo
+--                             AND pen.id_processo_evento_excludente IS NULL
+--                             and pen.id_evento = 51 -- esse é o codigo do movimento. se esse id mudar tem de ir na tb_evento_processual.cd_evento
+--                             )
+--              where pen.dt_atualizacao < doc.dt_juntada
+--              order by pen.dt_atualizacao desc
+--              limit 1
+--           ) concluso ON TRUE
 --         INNER JOIN tb_processo_evento iniciada_execucao
 --             ON (iniciada_execucao.id_processo = doc.id_processo
 --             AND iniciada_execucao.id_evento = 11385)
@@ -63,12 +64,12 @@ WITH RECURSIVE
           and doc.dt_juntada :: date between
             coalesce(:DATA_INICIAL_OPCIONAL,date_trunc('month', current_date))::date
             and (coalesce(:DATA_OPCIONAL_FINAL, current_date))::date
-
-          and concluso.ds_texto_final_interno ilike ANY (
-            ARRAY['Conclusos os autos para julgamento da ação incidental na execu__o%'
-                ,'Conclusos os autos para julgamento dos Embargos _ Execu__o%'
-                ,'Conclusos os autos para julgamento da Impugna__o _ Sentença de Liquida__o%']
-            )
+-- ver inner join do concluso
+--       and concluso.ds_texto_final_interno ilike ANY (
+--         ARRAY['Conclusos os autos para julgamento da ação incidental na execu__o%'
+--              ,'Conclusos os autos para julgamento dos Embargos _ Execu__o%'
+--              ,'Conclusos os autos para julgamento da Impugna__o _ Sentença de Liquida__o%']
+--         )
         -- TODO: pensar sobre se é necessário já testar os movimentos aqui
 --     AND EXISTS (
 --         SELECT 1 FROM
@@ -252,7 +253,7 @@ UNION ALL
          , to_char(incidentes_julgados.data_julgado, 'dd/MM/yyyy')     AS "Julgado em"
          , pt.nm_tarefa                                                as "Tarefa Atual"
          , incidentes_julgados.numero_julgados                                AS "Quantidade"
-         ,'$URL/execucao/T964?NUM_PROCESSO=' || p.nr_processo
+         ,'$URL/execucao/T965?NUM_PROCESSO=' || p.nr_processo
                                 -- MAGISTRADO='||incidentes_julgados.id_pessoa
                                 || '&DATA_INICIAL_OPCIONAL=' ||
                                     to_char(coalesce(:DATA_INICIAL_OPCIONAL, date_trunc('month', current_date))::date, 'mm/dd/yyyy')
