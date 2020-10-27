c36608b058b8350222c5b0da347c0f0dda86b451
Antonio Lucas Neres <antoniolucasnobar@gmail.com>
2020-10-27 20:39:48 -0300

    [N979][R144010] - 2020-10-22 - adicao do filtro por cargo do processo (nem todos tem chip j1/j2)

--- relatorios/consultas/N979-processos-com-chip-1g.sql
+++ relatorios/consultas/N979-processos-com-chip-1g.sql
@@ -1,10 +1,12 @@
 -- [R144010][N979] 2020-10-07 - adicao de novos filtros por etiqueta.
+-- [R144010] - 2020-10-22 - adicao do filtro por cargo do processo (nem todos tem chip j1/j2)
 with t1 as(
 --T1 contém a maioria das regras, somente não tem o filtro pela fase processual
 select
-	oj.ds_orgao_julgador as "Órgão Julgador",
+    REPLACE(oj.ds_orgao_julgador, 'VARA DO TRABALHO', 'VT' ) AS "Órgão Julgador",
 	ptrf.id_processo_trf,
-	classe.ds_classe_judicial as "Classe Judicial",
+	classe.ds_classe_judicial_sigla as "Classe Judicial",
+    cargo.cd_cargo as "Cargo",
 	procxtar.nm_tarefa as "Tarefa atual"
 from
 	tb_processo_trf ptrf
@@ -20,8 +22,11 @@ join
 	tb_etq_etiqueta_instancia eqi on (eqi.id_etq_etiqueta_instancia = peq.id_etq_etiqueta_instancia)
 join
 	tb_etq_etiqueta eq on (eq.id_etq_etiqueta = eqi.id_etq_etiqueta)
+inner join tb_orgao_julgador_cargo ojc ON (ptrf.id_orgao_julgador_cargo = ojc.id_orgao_julgador_cargo)
+inner join tb_cargo cargo ON (ojc.id_cargo = cargo.id_cargo)
 where
 	coalesce(:ORGAO_JULGADOR_TODOS, oj.id_orgao_julgador) = oj.id_orgao_julgador
+	AND  ((:CARGO is null) or (position(:CARGO in ojc.ds_cargo) > 0))
   AND  (
     CASE
         WHEN :ID_ETIQUETA IS NULL THEN TRUE
@@ -64,7 +69,8 @@ where
 	and ptrf.cd_processo_status = 'D'
     group by 	oj.ds_orgao_julgador,
 	ptrf.id_processo_trf,
-	classe.ds_classe_judicial,
+	classe.ds_classe_judicial_sigla,
+    cargo.cd_cargo,
 	procxtar.nm_tarefa
 )
 select
@@ -72,6 +78,7 @@ select
 	'http://processo='||p.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_TAREFA&texto='||p.nr_processo as "Processo" ,
 	t1."Órgão Julgador",
 	t1."Classe Judicial",
+	t1."Cargo",
     (SELECT string_agg(eq.nm_etiqueta || ' (' || to_char(peq.dh_inclusao, 'dd/mm/yyyy') || ')', ', ') FROM
         tb_etq_processo_etiqueta peq
             join
@@ -79,7 +86,6 @@ select
             join
         tb_etq_etiqueta eq on (eq.id_etq_etiqueta = eqi.id_etq_etiqueta)
      where peq.id_processo_trf = t1.id_processo_trf
-        order by peq.dh_inclusao
         ) AS "Chips/Etiquetas (data de inclusão)",
     fase.nm_agrupamento_fase as "Fase Processual",
 	t1."Tarefa atual"
