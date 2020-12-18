e71334ca0c7213bcd6b84ce6c944be92b84eadd1
Antonio <antonio.lucas@trt4.jus.br>
2020-12-18 16:38:39 -0300

    [R149090][T990] adicao de mais campos:
    - data do arquivamento definitivo
    - fase de cadastro e atual
    - correcao na data do cadastramento, que usava campo incorreto

--- relatorios/consultas/T990-CLE-por-VT.sql
+++ relatorios/consultas/T990-CLE-por-VT.sql
@@ -1,20 +1,36 @@
+-- [R149090][T990] Listagem de CLE
+
 SELECT 'http://processo='||cle.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_DETALHES_PROCESSO' as " ",
-        'http://processo='||cle.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_TAREFA&texto='||cj.ds_classe_judicial_sigla||' '||cle.nr_processo as "Processo",
-    REPLACE(oj.ds_orgao_julgador, 'VARA DO TRABALHO', 'VT') AS "Unidade",
-    cle.dt_inicio_clet as "Data de cadastro da CLE", 
-    cle.dt_ajuizamento as "Data de ajuizamento da ação",
-    fase.nm_agrupamento_fase as "Fase",
-    pt.nm_tarefa as "Tarefa"
+        'http://processo='||cle.nr_processo||'&grau=primeirograu&recurso=$RECURSO_PJE_TAREFA&texto='||cle.nr_processo as "Processo",
+    REPLACE(oj.ds_orgao_julgador, 'VARA DO TRABALHO', 'VT') AS "Unidade"
+    , cle.dh_cadastro_clet AS "Data Cadastro"
+    , fase_cadastro.nm_agrupamento_fase as "Fase de cadastro"
+    , fase_atual.nm_agrupamento_fase as "Fase Atual"
+    , pt.nm_tarefa as "Tarefa"
+    , CASE
+        WHEN fase_atual.id_agrupamento_fase = 5 AND pe.id_evento = 246 THEN pe.dt_atualizacao
+    END AS "Data Arquivamento definitivo"
 FROM
     tb_processo_clet cle
-    INNER JOIN tb_processo p ON p.id_processo = cle.id_processo_trf
-	inner join tb_processo_trf ptrf on ptrf.id_processo_trf = cle.id_processo_trf
-	inner join tb_orgao_julgador oj on oj.id_orgao_julgador = ptrf.id_orgao_julgador
-    inner join tb_processo_tarefa pt on pt.id_processo_trf = p.id_processo
-    inner join tb_agrupamento_fase fase on (p.id_agrupamento_fase = fase.id_agrupamento_fase)
-    INNER JOIN tb_classe_judicial cj ON (cj.id_classe_judicial = ptrf.id_classe_judicial)
+    INNER JOIN tb_processo p ON (cle.id_processo_trf = p.id_processo)
+    INNER JOIN tb_processo_trf ptrf ON (cle.id_processo_trf = ptrf.id_processo_trf)
+    INNER JOIN tb_orgao_julgador oj ON (oj.id_orgao_julgador = ptrf.id_orgao_julgador)
+    INNER JOIN tb_agrupamento_fase fase_cadastro on
+        (cle.id_fase_processual_inicio_clet = fase_cadastro.id_agrupamento_fase)
+    INNER JOIN tb_agrupamento_fase fase_atual on
+        (p.id_agrupamento_fase = fase_atual.id_agrupamento_fase)
+    INNER JOIN tb_processo_tarefa pt on pt.id_processo_trf = p.id_processo
+    LEFT JOIN LATERAL (
+        SELECT * FROM tb_processo_evento pe WHERE p.id_processo = pe.id_processo
+            AND pe.id_processo_evento_excludente is null
+            AND pe.id_evento IN (246, 245, 893)
+        ORDER BY pe.dt_atualizacao DESC
+        LIMIT 1
+        ) pe ON TRUE
 WHERE
-    cle.dt_inicio_clet:: date between (:DATA_INICIAL)::date and (:DATA_FINAL)::date
+    cle.dh_cadastro_clet::date BETWEEN
+            coalesce(:DATA_INICIAL_OPCIONAL, date_trunc('year', current_date))::date
+            AND (coalesce(:DATA_OPCIONAL_FINAL, current_date))::date
     AND oj.id_orgao_julgador = coalesce(:ORGAO_JULGADOR_TODOS, oj.id_orgao_julgador)
-    AND fase.id_agrupamento_fase = coalesce(:ID_FASE_PROCESSUAL, fase.id_agrupamento_fase)
-ORDER BY cle.dt_inicio_clet
+    AND fase_atual.id_agrupamento_fase = coalesce(:ID_FASE_PROCESSUAL, fase_atual.id_agrupamento_fase)
+ORDER BY cle.dh_cadastro_clet
